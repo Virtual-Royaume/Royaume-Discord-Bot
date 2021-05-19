acbe9bcb5d64075cf0c4cefb3a1f13fba76c97a6
Bluzzi <bluzzigame@gmail.com>
2021-05-19 18:58:34 +0200

    Client: instantiation of CommandMap and EventMap class, add messages in console...

--- src/Client.ts
+++ src/Client.ts
@@ -1,20 +1,37 @@
 import * as discord from "discord.js";
-import {readFileSync} from "fs";
+import { readFileSync } from "fs";
 
-class Client extends discord.Client {
+import Constants from "./constants/Constants";
+
+import EventMap from "./modules/EventMap";
+import CommandMap from "./modules/CommandMap";
+
+export default class Client {
+
     public static instance: discord.Client;
 
+    public eventMap: EventMap|null = null;
+    public commandMap: CommandMap|null = null;
+
     constructor(){
-        super();
-
+        // Create bot instance and login it :
         Client.instance = new discord.Client();
+        Client.instance.login(readFileSync(__dirname + "/resources/token.txt", {encoding: "utf-8"}));
 
-        Client.instance.login(readFileSync(__dirname + "/token.txt", {encoding: "utf-8"}));
-    }
+        Client.instance.on("ready", () => {
+            // Load events :
+            console.log(Constants.prefix + "Loading events...");
+            this.eventMap = new EventMap();
 
-    private loadEvents(){
+            //Load commands :
+            console.log(Constants.prefix + "Loading commands...");
+            this.commandMap = new CommandMap();
 
+            // Finish :
+            console.log(Constants.prefix + "The bot has been started !");
+        });
     }
 }
 
+console.log(Constants.prefix + "Sarting in progress...");
 new Client();

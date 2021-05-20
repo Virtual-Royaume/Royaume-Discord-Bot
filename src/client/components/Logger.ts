445abb52f231714bb0dff096b1e4b3714eec7c71
Bluzzi <bluzzigame@gmail.com>
2021-05-20 15:25:42 +0200

    Logger: fix return type

--- src/client/components/Logger.ts
+++ src/client/components/Logger.ts
@@ -10,27 +10,27 @@ export default class Logger {
         console.log(ColorManager[color](Constants.prefix + "[" + type.toUpperCase() + "] " + message));
     }
 
-    public info(message: string) : any {
+    public info(message: string) : void {
         this.log("yellow", "info", message);
     }
 
-    public notice(message: string) : any {
+    public notice(message: string) : void {
         this.log("yellow", "notice", message);
     }
 
-    public warning(message: string) : any {
+    public warning(message: string) : void {
         this.log("blue", "warning", message);
     }
 
-    public debug(message: string) : any {
+    public debug(message: string) : void {
         this.log("gray", "debug", message);
     }
 
-    public error(message: string) : any {
+    public error(message: string) : void {
         this.log("red", "error", message);
     }
 
-    public wow(message: string) : any {
+    public wow(message: string) : void {
         this.log("rainbow", "wow", message);
     }
 }

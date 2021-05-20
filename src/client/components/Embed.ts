e8fb34d502ac42fef9bef5599fb6bfaf27d30e66
Bluzzi <bluzzigame@gmail.com>
2021-05-20 12:03:05 +0200

    Create Embed client component

---
+++ src/client/components/Embed.ts
@@ -0,0 +1,15 @@
+import { TextChannel, DMChannel, MessageEmbed } from "discord.js";
+
+import Constants from "../../constants/Constants";
+
+export default class Embed {
+
+    public sendSimpleEmbed(message: string, channel: TextChannel|DMChannel){
+        let embed = new MessageEmbed();
+
+        embed.setColor(Constants.color);
+        embed.setDescription(message);        
+
+        channel.send(embed);
+    }
+}

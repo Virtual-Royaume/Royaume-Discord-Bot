875bf4fc103a3fbe3ba4daceee048a50003ac889
Bluzzi <bluzzigame@gmail.com>
2021-05-20 16:22:07 +0200

    CommandMap: remove tests...

--- src/modules/CommandMap.ts
+++ src/modules/CommandMap.ts
@@ -1,20 +1,8 @@
 import Client from "../client/Client";
-import { Message, TextChannel } from "discord.js";
 
 export default class CommandMap {
     
     constructor(){
-        /*fs.readdirSync(__dirname + "/events/").forEach(fileName => {
-            require("./events/" + fileName);
-            
-            console.log(prefix + "Event " + fileName + " chargé");
-        });*/
-
-        Client.instance.on("message", (msg: Message) => {
-            if(msg.channel instanceof TextChannel){
-                //Client.instance.embed.sendSimple("tgm", msg.channel);
-                console.log(msg.content);
-            }
-        });
+        //TODO
     }
 }

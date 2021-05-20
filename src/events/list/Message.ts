1075979b38c9a3b48b18dea72596aeca6d36262a
Bluzzi <bluzzigame@gmail.com>
2021-05-20 18:52:18 +0200

    Create Message event class in TS

---
+++ src/events/list/Message.ts
@@ -0,0 +1,14 @@
+import { Message as Msg, TextChannel } from "discord.js";
+
+import Client from "../../client/Client";
+
+import ChannelIDs from "../../constants/ChannelIDs";
+import Constants from "../../constants/Constants";
+
+import MessageMemberCount from "../../database/entities/MessageMemberCount";
+
+export default class Message {
+
+    public async run(message: Msg){
+    }
+}

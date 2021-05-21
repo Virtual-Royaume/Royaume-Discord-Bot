b25143da54cb3aef0ef0067d65908697d768a157
Sh4yn <guemmoudrayane03@gmail.com>
2021-05-21 23:25:06 +0200

    add games commands

---
+++ src/commands/list/games/fishington.ts
@@ -0,0 +1,46 @@
+import { Message } from "discord.js";
+import Client from "../../../client/Client";
+import Command from "../../Command";
+import Constants from "./../../../constants/Constants";
+
+export default class Youtube extends Command {
+
+    constructor(){
+        super(
+            "fishington",
+            "Permet de jouer à fishington ensemble",
+            "games"
+        );
+    }
+
+    public run(args: any[], message: Message): void {
+      if (!message.member?.voice.channelID) {
+        Client.instance.embed.sendSimple(
+          "Vous devez être dans un salon vocal !",
+          message.channel
+        );
+        return;
+      };
+
+      const instance: any = Client.instance;
+
+      instance.api
+        .channels(message.member.voice.channelID)
+        .invites.post({
+          data: {
+            temporary: true,
+            max_age: 7200, // 2h
+            max_uses: 0,
+            unique: true,
+            target_type: 2,
+            target_application_id: Constants.fishington,
+          }
+        })
+        .then((invite: {code: string}): void => {
+          Client.instance.embed.sendSimple(
+            `Rejoignez l'activité en cliquant sur le lien\nhttps://discord.gg/${invite.code}`,
+            message.channel
+          )
+        });
+    }
+}

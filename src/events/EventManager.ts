2a9c4cf5b539924b6a8d19ad971c52da87bc553a
RomainSav <romain.sv@icloud.com>
2021-05-21 17:05:21 +0200

    Add events autoloader

--- src/events/EventManager.ts
+++ src/events/EventManager.ts
@@ -1,12 +1,25 @@
-import Client from "../client/Client";
+import Client from "../client/Client";;
 
-import GuildMemberAdd from "./list/GuildMemberAdd";
-import Message from "./list/Message";
+import { readdir } from "fs";
+import path from 'path';
+import Event from "./Event";
 
 export default class EventManager {
 
     constructor(){
-        Client.instance.on("guildMemberAdd", guildMember => (new GuildMemberAdd()).run(guildMember));
-        Client.instance.on("message", message => (new Message()).run(message));
+        this.loadEvents();
+    }
+
+    private loadEvents() {
+        readdir(path.join(__dirname, 'list'), (err, files) => {
+            files.forEach((file) => {
+                delete require.cache[file];
+                try {
+                    const event: Event = new (require(path.join(__dirname, 'list', file)).default);
+                    Client.instance.on(event.name, (...args) => event.run(...args))
+                } catch (e) {
+                }
+            })
+        })
     }
 }

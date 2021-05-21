1cca6bb57ecf938550b0b914a5a461de446cfa3e
Bluzzi <bluzzigame@gmail.com>
2021-05-21 18:00:26 +0200

    CommandManager: clean command loader, fix syntax, remove useless methods...

--- src/commands/CommandManager.ts
+++ src/commands/CommandManager.ts
@@ -7,36 +7,50 @@ import Constants from "../constants/Constants";
 
 import Command from "./Command";
 
-import {readdir} from "fs";
-import path from 'path';
+import { readdirSync } from "fs";
+import path from "path";
 
 export default class CommandManager {
 
-    // public readonly commands: {[key: string]: Command} = {};
     public readonly commands: Collection<string, Command> = new Collection();
-    public readonly aliases: Collection<string, Command> = new Collection();
+    public readonly commandsAliases: Collection<string, Command> = new Collection();
+
     public readonly categories: string[] = [];
+
     public readonly categoriesWithCommands: Collection<string, Collection<string, Command>> = new Collection();
 
-    constructor() {
-        // Register commands automatically
-        readdir(path.join(__dirname, 'list'), (err, categories) => {
-            categories.forEach((category) => {
+    constructor(){
         // Register commands automatically :
         readdirSync(path.join(__dirname, "list")).forEach(category => {
             let commandsOfCategory = readdirSync(path.join(__dirname, "list", category));
 
-            commandsOfCategory.filter(file => file.endsWith(".ts")).forEach(command => {
-                this.loadCommand(path.join(__dirname, "list", category, command), category);
+            commandsOfCategory.filter(file => file.endsWith(".ts")).forEach(commandFile => {
+                const commandPath = path.join(__dirname, "list", category, commandFile);
+                const command: Command = new (require(commandPath).default);
+
+                // Save command instance by name :
+                this.commands.set(command.name, command);
+
+                // Save command instance by aliases :
+                command.aliases.forEach(alias => this.commandsAliases.set(alias, command));
+
+                // Check if the command category exist and add it if it does not exist :
+                if(!this.categories.includes(command.category)){
+                    this.categories.push(command.category)
+                    this.categoriesWithCommands.set(command.category, new Collection<string, Command>());
+                }
+
+                // Add command instance in category :
+                this.categoriesWithCommands.get(command.category)?.set(command.name, command);
             })
         });
 
         // Call commands methods :
         Client.instance.on("message", message => {
-            if (!message.content.startsWith(Constants.commandPrefix)) return;
-            if (message.content.length <= Constants.commandPrefix.length) return;
+            if(!message.content.startsWith(Constants.commandPrefix)) return;
+            if(message.content.length <= Constants.commandPrefix.length) return;
 
-            if (message.channel.id !== ChannelIDs.command) {
+            if(message.channel.id !== ChannelIDs.command){
                 Client.instance.embed.sendSimple(
                     "Vous ne pouvez pas faire de commande en dehors du salon <#" + ChannelIDs.command + ">.",
                     <TextChannel>message.channel
@@ -44,48 +58,14 @@ export default class CommandManager {
                 return;
             }
 
-            let args: string[] = message.content.split(" ");
-            let commandString = args.shift()?.substring(Constants.commandPrefix.length).toLowerCase();
-            // @ts-ignore
-            let command =  this.getCommandByName(commandString) || this.getCommandByAlias(commandString);
+            const args: string[] = message.content.split(" ");
+            const commandName: string|undefined = args.shift()?.substring(Constants.commandPrefix.length).toLowerCase();
 
-            if (command) {
-                command.run(args, message)
-            }
+            if(!commandName) return;
+
+            let command = this.commands.get(commandName) || this.commandsAliases.get(commandName);
+
+            if(command) command.run(args, message);
         });
-    }
-
-    public getCommandByName(commandName: string) : Command | undefined | null {
-        return typeof this.commands.get(commandName) !== "undefined" ? this.commands.get(commandName) : null;
-    }
-
-    public getCommandByAlias(commandName: string) : Command | undefined | null {
-        return typeof this.aliases.get(commandName) !== "undefined" ? this.aliases.get(commandName) : null;
-    }
-
-    public getCategoryCommands(categoryName: string) : Collection<string, Command> | undefined | null {
-        return typeof this.categoriesWithCommands.get(categoryName) !== "undefined" ? this.categoriesWithCommands.get(categoryName) : null;
-    }
-
-    private loadCommand(commandPath: string, commandName: string, commandCategory: string): void {
-        try {
-            const cmd: Command = new (require(`${commandPath}${path.sep}${commandName}`).default);
-            this.commands.set(cmd.getName(), cmd)
-            //Client.instance.logger.info(`Command ${cmd.getName()} has been loaded !`)
-            cmd.getAliases().forEach((alias) => {
-                this.aliases.set(alias, cmd)
-            })
-
-            if (!this.categories.includes(commandCategory)) {
-                this.categories.push(commandCategory)
-                this.categoriesWithCommands.set(commandCategory, new Collection<string, Command>())
-            }
-
-            // @ts-ignore
-            this.categoriesWithCommands.get(commandCategory).set(cmd.getName(), cmd);
-
-        } catch (e) {
-            Client.instance.logger.error(`Error: ${e}`)
-        }
     }
 }

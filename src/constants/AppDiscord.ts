c2e1ae4c2f36683f2b0350ed66f8a16d8848c5ad
Bluzzi <bluzzigame@gmail.com>
2021-05-22 17:50:09 +0200

    Create AppDiscord constants class (for Discord voice channel integrations)

---
+++ src/constants/AppDiscord.ts
@@ -0,0 +1,11 @@
+export default class AppDiscord {
+    
+    // Utility :
+    public static youtubeTogether: string = "755600276941176913";
+
+    // Games :
+    public static betrayal: string = "773336526917861400";
+    public static pokerNight: string = "755827207812677713";
+    public static fishington: string = "814288819477020702";
+}
+

1a113e1c239030c5cfff620ced0ba7a5e35cf787
Bluzzi <bluzzigame@gmail.com>
2021-05-22 17:50:24 +0200

    Constants: remove discord voice channel integrations

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -4,9 +4,4 @@ export default class Constants {
     public static commandPrefix: string = "-";
 
     public static color: string = "#7F52DC";
-
-    public static youtubeTogether: string = "755600276941176913";
-    public static betrayal: string = "773336526917861400";
-    public static pokernight: string = "755827207812677713";
-    public static fishington: string = "814288819477020702";
 }

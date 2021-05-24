5a78472bba0e31dad69e0d0c800579e6193cfbfc
Bluzzi <camilledugas.pro@gmail.com>
2021-05-24 15:42:41 +0200

    Command: support for AdditionalCommandParams

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,20 +1,35 @@
 import { Message } from "discord.js";
+import Client from "../client/Client";
+import Constants from "../constants/Constants";
+import AdditionalCommandParams from "./AdditionalCommandParams";
 
 export default abstract class Command {
 
+    // Base properties :
     public readonly name: string;
     public readonly description: string;
     public readonly category: string;
-    public readonly aliases: string[];
-    public readonly usage: string;
+    public readonly additionalParams: AdditionalCommandParams;
 
-    constructor(name: string, description: string, category: string, aliases: string[] = [], usage: string = ''){
+    // Others :
+    private formattedUsage: string = "";
+
+    constructor(name: string, description: string, category: string, additionalParams: AdditionalCommandParams = {}){
         this.name = name;
         this.description = description;
         this.category = category;
-        this.aliases = aliases;
-        this.usage = usage;
+        this.additionalParams = additionalParams;
+        
+        // Formatting of the usage if there is one :
+        if(this.additionalParams.usage && this.additionalParams.usage.length > 0){
+            this.additionalParams.usage.forEach(usageParam => {
+                let symbols = usageParam.type === "optional" ? ["<", ">"] : ["[", "]"];
+
+                this.formattedUsage += " " + symbols[0] + usageParam.usage + symbols[1];
+            });
+        }
     }
 
     public abstract run(args: any[], message: Message) : void;
+
 }

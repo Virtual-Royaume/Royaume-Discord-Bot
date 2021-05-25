9883b08cc5ebcb8eb126ad7aa5311d6e9da0b999
Bluzzi <camilledugas.pro@gmail.com>
2021-05-25 15:41:57 +0200

    EventManager: update
    - clean event autoloader
    - remove async in constructor (everything must be loaded in the right way)
    - add property to save events instance correctely, count events...

--- src/events/EventManager.ts
+++ src/events/EventManager.ts
@@ -1,31 +1,27 @@
 import Client from "../client/Client";;
-
-import { readdir } from "fs";
+import { readdirSync } from "fs";
 import path from 'path';
 import Event from "./Event";
+import { Collection } from "discord.js";
 
 export default class EventManager {
 
-    public readonly events: string[] = [];
+    public readonly events: Collection<string, Event[]> = new Collection();
+    public readonly eventListenerCount: number = 0;
 
     constructor(){
-        this.loadEvents();
-    }
+        const eventFiles = readdirSync(path.join(__dirname, "list"));
 
-    private loadEvents() {
-        readdir(path.join(__dirname, 'list'), (err, files) => {
-            files.forEach((file) => {
-                delete require.cache[file];
-                try {
-                    const event: Event = new (require(path.join(__dirname, 'list', file)).default);
-                    Client.instance.on(event.name, (...args) => event.run(...args))
-                    if (!this.events.includes(event.name)) {
-                        this.events.push(event.name);
-                    }
-                } catch (e) {
-                    Client.instance.logger.error(`Error: ${e.message}`)
-                }
-            })
-        })
+        eventFiles.forEach(file => {
+            delete require.cache[file];
+
+            const event: Event = new (require(path.join(__dirname, "list", file)).default);
+
+            // @ts-ignore : it can't be undefined because I check if it is defined on the same line
+            this.events.set(event.name, this.events.get(event.name) ? this.events.get(event.name).concat(event) : [event]);
+            Client.instance.on(event.name, (...args) => event.run(...args));
+            // @ts-ignore : we are in the constructor
+            this.eventListenerCount++;
+        });
     }
 }

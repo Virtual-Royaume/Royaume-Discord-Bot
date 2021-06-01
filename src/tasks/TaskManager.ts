ca03ddf2d71cba9b07f5451f225d1c70080999fb
Bluzzi <camilledugas.pro@gmail.com>
2021-06-01 16:27:52 +0200

    Create TaskManager

---
+++ src/tasks/TaskManager.ts
@@ -0,0 +1,21 @@
+import { readdirSync } from "fs";
+import path from "path";
+import Task from "./Task";
+
+export default class TaskManager {
+
+    public readonly tasks: Task[];
+    public readonly taskListenerCount: number = 0;
+
+    constructor(){
+        readdirSync(path.join(__dirname, "list")).forEach(file => {
+            const task: Task = new (require(path.join(__dirname, "list", file)).default);
+        
+            const interval: NodeJS.Timeout = setInterval(() => task.run(interval), task.interval);
+
+            // @ts-ignore : we are in the constructor
+            this.taskListenerCount++;
+            this.tasks.push(task);
+        });
+    }
+}

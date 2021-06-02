fe1ae3456d28e0c5ddbbf357e6e8ab25285c913a
Bluzzi <camilledugas.pro@gmail.com>
2021-06-02 21:29:57 +0200

    Create ActivityUpdate task

---
+++ src/tasks/list/ActivityUpdate.ts
@@ -0,0 +1,39 @@
+import Client from "../../client/Client";
+import ChannelIDs from "../../constants/ChannelIDs";
+import Task from "../Task"
+
+export default class ServerActivityUpdate extends Task {
+
+    private vocalInLastMinute: string[] = [];
+
+    constructor(){
+        super(60000 /* 1 minute */);
+    }
+
+    public async run(timeout: NodeJS.Timeout) : Promise<void> {
+        const serverActivity = await Client.instance.getServerActivity();
+
+        // Update daily member count :
+        if(Client.instance.getGuild().memberCount > serverActivity.memberCount){
+            serverActivity.memberCount = Client.instance.getGuild().memberCount;
+        }
+
+        // Update voice time (member, server global) :
+        for(let i = this.vocalInLastMinute.length; i > 0; i--){
+            serverActivity.voiceMinute++;
+            
+            let userId = this.vocalInLastMinute.shift();
+
+            //TODO : update UserActivity
+        }
+
+        Client.instance.getGuild().voiceStates.cache.forEach(voiceState => {
+            if(voiceState.member && !voiceState.selfMute && voiceState.channel?.id !== ChannelIDs.afk){
+                this.vocalInLastMinute.push(voiceState.member.id);
+            }
+        });
+
+        // Save data :
+        serverActivity.save();
+    }
+}

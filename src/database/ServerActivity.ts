829b5b68de26192dfdb9b69a291a724b4f24cfe5
Bluzzi <camilledugas.pro@gmail.com>
2021-06-04 21:09:43 +0200

    ServerActivity (entity): fix dayjs import

--- src/database/ServerActivity.ts
+++ src/database/ServerActivity.ts
@@ -1,11 +1,11 @@
 import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
-import daysjs from "dayjs";
+import dayjs from "dayjs";
 
 @Entity()
 export default class ServerActivity extends BaseEntity {
 
     @PrimaryColumn({type: "date"})
-    date: string = daysjs().format("YYYY-MM-DD");
+    date: string = dayjs().format("YYYY-MM-DD");
 
     @Column()
     voiceMinute: number = 0;
@@ -20,7 +20,7 @@ export default class ServerActivity extends BaseEntity {
     private static serverActivity: ServerActivity|undefined;
 
     public static async getServerActivity() : Promise<ServerActivity> {
-        const date = daysjs().format("YYYY-MM-DD");
+        const date = dayjs().format("YYYY-MM-DD");
 
         if(!this.serverActivity){
             this.serverActivity = await ServerActivity.findOne({date: date});

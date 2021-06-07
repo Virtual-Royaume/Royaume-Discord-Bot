ebf3b629db7541b32274dd094c631ab61a0df55d
Se7en <yt.seven71@gmail.com>
2021-06-07 21:25:34 +0200

    ChannelIDs update
    - Changed ChannelIDs class to an enum.
    - Added (every) text channel

--- src/constants/ChannelIDs.ts
+++ src/constants/ChannelIDs.ts
@@ -1,9 +1,18 @@
-export default class ChannelIDs {
-    
-    public static general: string = "786216771723198514";
-    public static command: string = "736677048214618213";
-    public static verif: string = "837678344451129404";
+export enum ChannelIDs {
+    //Text channels
+    annonces = "797103954768298005",
+    general = "786216771723198514",
+    commandes = "736677048214618213",
+    verif = "837678344451129404",
+    games = "778044698685866025",
+    musique = "829662265942343692",
+    dropshipping = "798164233443213322",
+    developpement = "732392873667854372",
+    trading = "779129024327712783",
+    graphisme = "768996501049311243",
+    sneakers = "789126328082235412",
 
-    public static afk: string = "832042763675631688";
+    //Voice channels
+    afk = "832042763675631688"
 }
 

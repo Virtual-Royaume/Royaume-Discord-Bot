fdfe36f75c856240ad641e20eef36ced478e4557
Bluzzi <camilledugas.pro@gmail.com>
2021-06-16 13:50:20 +0200

    Embed (client component): change method name (sendSimple() -> send()) and add options param

--- src/client/components/Embed.ts
+++ src/client/components/Embed.ts
@@ -1,14 +1,25 @@
-import {TextChannel, DMChannel, MessageEmbed, Message, NewsChannel} from "discord.js";
+import {TextChannel, DMChannel, MessageEmbed, Message, NewsChannel, MessageAttachment, FileOptions} from "discord.js";
 
 import Constants from "../../constants/Constants";
 
+interface EmbedOptions {
+    title?: string,
+    image?: FileOptions
+}
+
 export default class Embed {
 
-    public sendSimple(message: string, channel: TextChannel|DMChannel|NewsChannel) : Promise<Message> {
+    public send(message: string, channel: TextChannel|DMChannel|NewsChannel, options: EmbedOptions|null = null) : Promise<Message> {
         const embed = new MessageEmbed();
 
         embed.setColor(Constants.color);
         embed.setDescription(message);        
+
+        if(options?.title) embed.setTitle(options.title);
+        if(options?.image){
+            embed.attachFiles([options.image]);
+            embed.setImage("attachment://" + (embed.files[0] as FileOptions).name);
+        }
 
         return channel.send(embed);
     }

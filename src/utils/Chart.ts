2a567ea8e28f2a764c4554d554e780ec0770fb51
Bluzzi <camilledugas.pro@gmail.com>
2021-06-18 14:27:31 +0200

    Chart (util): add "watermark"

--- src/utils/Chart.ts
+++ src/utils/Chart.ts
@@ -21,22 +21,50 @@ export default class Chart {
                     data: data
                 }]
             },
-            plugins: [{
-                id: "background-color",
-                beforeDraw: chart => {
-                    const ctx = chart.canvas.getContext("2d");
+            plugins: [
+                {
+                    id: "background-color",
+                    beforeDraw: chart => {
+                        const ctx = chart.canvas.getContext("2d");
 
-                    if(ctx){
-                        ctx.save();
-                        
-                        ctx.globalCompositeOperation = "destination-over";
-                        ctx.fillStyle = "white";
+                        if(ctx){
+                            ctx.save();
+                            
+                            ctx.globalCompositeOperation = "destination-over";
+                            ctx.fillStyle = "white";
 
-                        ctx.fillRect(0, 0, chart.width, chart.height);
-                        ctx.restore();   
+                            ctx.fillRect(0, 0, chart.width, chart.height);
+                            ctx.restore();   
+                        }
+                    }
+                },
+                {
+                    id: "signature",
+                    beforeDraw: chart => {
+                        const ctx = chart.canvas.getContext("2d");
+
+                        if(ctx){
+                            const signature = "royaume.world/discord";
+                            const signatureSize = ctx.measureText(signature);
+                            const signaturePos = {
+                                x: chart.width - signatureSize.width,
+                                y: 10,
+                            }
+
+                            ctx.restore();
+
+                            ctx.font = "12px sans-serif";
+                            ctx.fillStyle = "#929292";
+                            ctx.textAlign = "center";
+                            ctx.textBaseline = "top";
+
+                            ctx.fillText("royaume.world/discord", signaturePos.x, signaturePos.y);
+
+                            ctx.save();
+                        }
                     }
                 }
-            }]
+            ]
         }
 
         return config;

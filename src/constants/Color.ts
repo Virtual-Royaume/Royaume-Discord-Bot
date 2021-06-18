9a5650bfb063c900f1ac9e2fc1f91fc481b444ff
Bluzzi <camilledugas.pro@gmail.com>
2021-06-18 14:41:55 +0200

    Create new constants file for colors

---
+++ src/constants/Color.ts
@@ -0,0 +1,6 @@
+export enum Color {
+    blue = "#5339DD", // #7F52DC
+    red = "#FF5555",
+}
+
+export const defaultColor = Color.blue;
--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -2,6 +2,4 @@ export default class Constants {
 
     public static prefix: string = "[ROYAUME BOT] "; 
     public static commandPrefix: string = "-";
-
-    public static color: string = "#5339DD"; // #7F52DC
 }

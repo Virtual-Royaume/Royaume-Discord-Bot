d4fed2d015d8f49e216523aa5e23d913c1a0493d
Bluzzi <camilledugas.pro@gmail.com>
2021-07-02 12:42:42 +0200

    Inactive (command): adapt for new DB

--- src/commands/list/stats/Inactive.ts
+++ src/commands/list/stats/Inactive.ts
@@ -1,6 +1,6 @@
-import { GuildMember, Message, TextChannel } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import Client from "../../../client/Client";
-import MemberActivity from "../../../database/member/MemberActivity";
+import Member from "../../../database/src/models/Member";
 import Command from "../../Command";
 
 export default class Inactive extends Command {
@@ -14,16 +14,11 @@ export default class Inactive extends Command {
     }
 
     public async run(args: any[], message: Message) : Promise<void> {
-        const inactives = await MemberActivity.find({
-            where: {
-                monthMessageCount: 0
-            },
-            take: 30
-        });
+        const inactives = await Member.MemberModel.find({"activity.monthMessageCount": 0, alwaysInTheServer: true}).limit(30).exec();
 
         if(inactives.length){
             Client.instance.embed.sendSimple(
-                "**__Liste des membres inactif__**\n\n" + inactives.map(element => "<@" + element.userId + ">").join(", ") + ".", 
+                "**__Liste des membres inactif__**\n\n" + inactives.map(element => "<@" + element._id + ">").join(", ") + ".", 
                 <TextChannel>message.channel
             );
         } else {

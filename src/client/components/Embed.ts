a322d7af10de913c1fb8f0233b9e63d91bc66f48
Bluzzi <camilledugas.pro@gmail.com>
2022-05-18 04:07:40 +0200

    Embed (client component): fix typing and djs compatibility

--- src/client/components/Embed.ts
+++ src/client/components/Embed.ts
@@ -7,9 +7,11 @@ export default class Embed {
     public sendSimple(message: string, channel: TextChannel|DMChannel|NewsChannel) : Promise<Message> {
         const embed = new MessageEmbed();
 
+        // @ts-ignore : compatible with the type HexColorString in ColorResolvable, 
+        // but not detected because of the use of a variable
         embed.setColor(Constants.color);
         embed.setDescription(message);        
 
-        return channel.send(embed);
+        return channel.send({ embeds: [embed] });
     }
 }

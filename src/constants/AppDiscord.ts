44aa5d93fad4d700e443f0fa3f70c27ef4603f45
Bluzzi <camilledugas.pro@gmail.com>
2022-05-18 04:44:00 +0200

    AppDiscord: add new applications

--- src/constants/AppDiscord.ts
+++ src/constants/AppDiscord.ts
@@ -1,10 +1,24 @@
 export default class AppDiscord {
     
     // Utility :
-    public static youtubeTogether: string = "755600276941176913";
+    public static youtubeTogether = "880218394199220334";
 
     // Games :
-    public static betrayal: string = "773336526917861400";
-    public static pokerNight: string = "755827207812677713";
-    public static fishington: string = "814288819477020702";
+    public static betrayal = "773336526917861400";
+    public static pokerNight = "755827207812677713";
+    public static fishington = "814288819477020702";
+
+    // Games : // TODO : test this games
+    public static chess = "832012774040141894";
+    public static chessDev = "832012586023256104";
+    public static lettertile = "879863686565621790";
+    public static wordsnack = "879863976006127627";
+    public static doodlecrew = "878067389634314250";
+    public static awkword = "879863881349087252";
+    public static spellcast = "852509694341283871";
+    public static checkers = "832013003968348200";
+    public static puttparty = "763133495793942528";
+    public static sketchheads = "902271654783242291";
+    public static ocho = "832025144389533716";
+    public static sketchyartist = "879864070101172255";
 }

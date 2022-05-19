75fe532cbca7d3e0fb382e92028ba089ff90e73e
Bluzzi <camilledugas.pro@gmail.com>
2022-05-19 14:13:26 +0200

    GuildMemberAdd: create member with API

--- src/events/list/GuildMemberAdd.ts
+++ src/events/list/GuildMemberAdd.ts
@@ -2,6 +2,8 @@ import { GuildMember } from "discord.js";
 import Client from "../../Client";
 import Event from "../Event";
 import { verifRole } from "../../../resources/config/information.json";
+import { request } from "../../api/Request";
+import { createMember } from "../../api/requests/Member";
 
 export default class GuildMemberAdd extends Event {
 
@@ -10,10 +12,16 @@ export default class GuildMemberAdd extends Event {
     public async execute(member: GuildMember) : Promise<void> {
         if(member.user.bot) return;
 
+        // Add verification role :
         const role = await Client.instance.getGuild().roles.fetch(verifRole);
 
         if(role) member.roles.add(role);
 
-        // TODO : create the member with API 
+        // Create the member if he dosen't exist :
+        request(createMember, {
+            id: member.id,
+            username: member.user.username,
+            profilPicture: member.user.avatarURL
+        });
     }
 }

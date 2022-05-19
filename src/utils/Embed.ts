c2f7842a76af29965cc95695cbd8f869c59774cd
Bluzzi <camilledugas.pro@gmail.com>
2022-05-19 21:35:39 +0200

    Embed (utils): add title param for simpleEmbed() function

--- src/utils/Embed.ts
+++ src/utils/Embed.ts
@@ -3,10 +3,11 @@ import information from "../../resources/config/information.json";
 
 type EmbedType = "normal" | "error";
 
-export function simpleEmbed(message: string, type: EmbedType = "normal") : MessageEmbed {
+export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string) : MessageEmbed {
     return new MessageEmbed()
         .setDescription(message)
         // @ts-ignore : compatible with the type HexColorString in ColorResolvable, 
         // but not detected because of the use of a variable
-        .setColor(type === "normal" ? information.colors.primary : information.colors.error);
+        .setColor(type === "normal" ? information.colors.primary : information.colors.error)
+        .setTitle(title ?? "");
 }

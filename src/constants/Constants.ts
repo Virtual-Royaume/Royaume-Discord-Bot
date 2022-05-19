ed0a54112d5ee98804071fdac88eb5b76735223f
Bluzzi <camilledugas.pro@gmail.com>
2022-05-19 04:19:05 +0200

    Remove constants

--- src/constants/Constants.ts
+++
@@ -1,7 +0,0 @@
-export default class Constants {
-
-    public static prefix: string = "[ROYAUME BOT] "; 
-    public static commandPrefix: string = "+";
-
-    public static color: string = "#5339DD"; // #7F52DC
-}

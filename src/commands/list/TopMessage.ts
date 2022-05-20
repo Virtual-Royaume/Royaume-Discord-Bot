718c0afd880cef581c3542963b660b58d7f8b1ef
Bluzzi <camilledugas.pro@gmail.com>
2022-05-20 17:01:00 +0200

    TopMessage (command): rewrite for the new API

--- src/commands/list/TopMessage.ts
+++ src/commands/list/TopMessage.ts
@@ -1,8 +1,26 @@
 import { SlashCommandBuilder, SlashCommandChannelOption, SlashCommandNumberOption, SlashCommandStringOption } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+import { ChannelTypes } from "discord.js/typings/enums";
+import { request } from "../../api/Request";
+import { getChannelMessageCount, getMonthMessageCount, getTotalMessageCount, getVoiceTime } from "../../api/requests/Member";
+import { Member } from "../../api/Schema";
+import { simpleEmbed } from "../../utils/Embed";
 import Command from "../Command";
 
+type Source = "total" | "mois" | "salon";
+
+interface SourceChoice {
+	name: string;
+	value: Source;
+}
+
 export default class TopMessage extends Command {
+
+    private sourceChoices: SourceChoice[] = [
+		{ name: "Total", value: "total" },
+		{ name: "Mois", value: "mois" },
+		{ name: "Salon", value: "salon" }
+	];
 
     public readonly slashCommand = new SlashCommandBuilder()
         .setName("top-message")
@@ -10,11 +28,7 @@ export default class TopMessage extends Command {
         .addStringOption(new SlashCommandStringOption()
             .setName("source")
             .setDescription("Source du classement")
-            .addChoices(
-                { name: "total", value: "total" },
-                { name: "mois", value: "mois" },
-                { name: "salon", value: "salon" }
-            )
+            .addChoices(...this.sourceChoices)
             .setRequired(true)
         ).addNumberOption(new SlashCommandNumberOption()
             .setName("page")
@@ -23,68 +37,92 @@ export default class TopMessage extends Command {
         ).addChannelOption(new SlashCommandChannelOption()
             .setName("salon")
             .setDescription("Choix du salon si vous avez choisi \"salon\" comme source")
+            // @ts-ignore : DJS - DJS/builders typing version problem
+			.addChannelTypes(ChannelTypes.GUILD_TEXT)
         );
 
     public readonly defaultPermission: boolean = true;
 
-    public execute(command: CommandInteraction) : void {
-        // Get message category (specific channel, total, month) :
-        // let columnName: string;
-        // let category: string;
+    private memberPerPage = 20;
 
-        // if((!["total", "mois"].includes(args[0]) && !(message.mentions.channels.first() instanceof TextChannel))){
-        //     Client.instance.embed.sendSimple(this.getFormattedUsage(), message.channel);
+    public async execute(command: CommandInteraction) : Promise<void> {
+        const source: Source = <Source>command.options.getString("source", true);
+        let page = command.options.getNumber("page") ?? 1;
 
-        //     return;
-        // } else {
-        //     const channel = message.mentions.channels.first();
+        // Get data and sort it :
+        interface Data {
+            username: string;
+            messageCount: number;
+        }
 
-        //     if(channel instanceof TextChannel){
-        //         columnName = MemberActivity.channelIdsToColumnName[channel.id];
-        //         category = "<#" + channel.id + ">";
-        //     } else {
-        //         columnName = args[0] === "mois" ? "monthMessageCount" : "totalMessageCount";
-        //         category = args[0] === "mois" ? "mois" : "total";
-        //     }
-        // }
+        let members: Data[] = [];
 
-        // // Get page :
-        // const memberPerPage = 20;
-        // const totalRows = await MemberActivity.count();
-        // const maxPage = Math.ceil(totalRows / memberPerPage);
+        switch(source){
+            case "mois":
+                members = (await request<{ members: Member[] }>(getMonthMessageCount)).members.sort((a, b) => {
+                    return (b?.activity.messages.monthCount ?? 0) - (a?.activity.messages.monthCount ?? 0);
+                }).map(member => {
+                    return {
+                        username: member.username,
+                        messageCount: member.activity.messages.monthCount
+                    }
+                });
+            break;
 
-        // let page = 1;
+            case "total":
+                members = (await request<{ members: Member[] }>(getTotalMessageCount)).members.sort((a, b) => {
+                    return (b?.activity.messages.totalCount ?? 0) - (a?.activity.messages.totalCount ?? 0);
+                }).map(member => {
+                    return {
+                        username: member.username,
+                        messageCount: member.activity.messages.totalCount
+                    }
+                });
+            break;
 
-        // if(args[1] && !isNaN(args[1]) && page > 0){
-        //     page = args[1] > maxPage ? maxPage : Math.abs(args[1]);
-        // }
-        
-        // // Send scoreboard :
-        // const topVoiceOfPage = (await MemberActivity.find({
-        //     order: {
-        //         [columnName]: "DESC"
-        //     },
-        //     skip: (page - 1) * memberPerPage,
-        //     take: memberPerPage
-        // }));
+            case "salon":
+                const channel = command.options.getChannel("salon");
 
-        // let scorebordMessage = "__**Classements selon le nombre de message (" + category + ") (page : " + page + "/" + maxPage + ")**__\n\n";
+                if(!channel){
+                    command.reply({ embeds: [simpleEmbed("Vous devez mentionner un salon.", "error")], ephemeral: true });
+				    return;		
+                }
 
-        // for(let i = 0; i < topVoiceOfPage.length; i++){
-        //     const member = topVoiceOfPage[i];
-        //     const guildMember = Client.instance.getGuild().members.cache.get(member.userId);
-            
-        //     let memberName: string;
+                members = (await request<{ members: Member[] }>(getChannelMessageCount)).members.sort((a, b) => {
+                    const aChannel = a.activity.messages.perChannel.find(c => channel.id === c?.channelId);
+                    const bChannel = b.activity.messages.perChannel.find(c => channel.id === c?.channelId);
 
-        //     if(guildMember){
-        //         memberName = guildMember.displayName;
-        //     } else {
-        //         memberName = (await Member.findOne({userId: member.userId})).username;
-        //     }
+                    return (bChannel?.messageCount ?? 0) - (aChannel?.messageCount ?? 0);
+                }).map(member => {
+                    const selectChannel = member.activity.messages.perChannel.find(c => channel.id === c?.channelId);
 
-        //     scorebordMessage += "**" + (i + 1 + (page - 1) * memberPerPage) + ". " + memberName + " :** " + member[columnName] + "\n";
-        // }
+                    return {
+                        username: member.username,
+                        messageCount: selectChannel?.messageCount ?? 0
+                    }
+                });
+            break;
+        }
 
-        // Client.instance.embed.sendSimple(scorebordMessage, message.channel);   
+        // Get page and max page :
+        const maxPage = Math.ceil(members.length / this.memberPerPage);
+        page = page > maxPage ? maxPage : page;
+
+        // Slice the members with page and max page :
+        members = members.slice(page * this.memberPerPage - this.memberPerPage, page * this.memberPerPage);
+
+        // Format leaderboard :
+        let message = "";
+
+        for(let i = 0; i < members.length; i++){
+            const member = members[i];
+
+            message += `**${(i + 1 + (page - 1) * this.memberPerPage)}. ${member.username} :** ${member.messageCount}\n`;
+        }
+
+        // Send leaderboard :
+        command.reply({
+            embeds: [simpleEmbed(message, "normal", `Classements des membres les plus actifs (${source}) (page : ${page}/${maxPage})`)]
+        });
     }
 }

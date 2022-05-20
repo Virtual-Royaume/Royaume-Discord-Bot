610f9cee918103e889c9a0003c508c9faf51c463
Bluzzi <camilledugas.pro@gmail.com>
2022-05-20 05:39:31 +0200

    Update GraphQL Schema

--- src/api/Schema.ts
+++ src/api/Schema.ts
@@ -1,8 +1,14 @@
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
     ID: string;
@@ -14,147 +20,129 @@ export type Scalars = {
 };
 
 export type ChannelMessageCount = {
-    __typename?: 'ChannelMessageCount';
-    channelId: Scalars['String'];
-    messageCount: Scalars['Int'];
+    __typename?: "ChannelMessageCount";
+    channelId: Scalars["String"];
+    messageCount: Scalars["Int"];
 };
 
 export type DiscordActivity = {
-    __typename?: 'DiscordActivity';
+    __typename?: "DiscordActivity";
     messages: DiscordMessageActivity;
-    voiceMinute: Scalars['Int'];
+    voiceMinute: Scalars["Int"];
 };
 
 export type DiscordMessageActivity = {
-    __typename?: 'DiscordMessageActivity';
-    monthCount: Scalars['Int'];
+    __typename?: "DiscordMessageActivity";
+    monthCount: Scalars["Int"];
     perChannel: Array<Maybe<ChannelMessageCount>>;
-    totalCount: Scalars['Int'];
+    totalCount: Scalars["Int"];
 };
 
 export type MainChannel = {
-    __typename?: 'MainChannel';
-    category: Scalars['String'];
-    channelId: Scalars['ID'];
+    __typename?: "MainChannel";
+    category: Scalars["String"];
+    channelId: Scalars["ID"];
 };
 
 export type MainRole = {
-    __typename?: 'MainRole';
-    category: Scalars['String'];
-    roleId: Scalars['ID'];
+    __typename?: "MainRole";
+    category: Scalars["String"];
+    roleId: Scalars["ID"];
 };
 
 export type Member = {
-    __typename?: 'Member';
-    _id: Scalars['String'];
+    __typename?: "Member";
+    _id: Scalars["String"];
     activity: DiscordActivity;
-    isOnServer: Scalars['Boolean'];
-    profilPicture: Scalars['String'];
-    username: Scalars['String'];
+    isOnServer: Scalars["Boolean"];
+    profilPicture: Scalars["String"];
+    username: Scalars["String"];
 };
 
 export type MemberInput = {
-    isOnServer?: InputMaybe<Scalars['Boolean']>;
-    profilPicture?: InputMaybe<Scalars['String']>;
-    username?: InputMaybe<Scalars['String']>;
+    isOnServer?: InputMaybe<Scalars["Boolean"]>;
+    profilPicture?: InputMaybe<Scalars["String"]>;
+    username?: InputMaybe<Scalars["String"]>;
 };
 
 export type Mutation = {
-    __typename?: 'Mutation';
-    addChannel: Scalars['Boolean'];
-    addRole: Scalars['Boolean'];
+    __typename?: "Mutation";
+    addChannel: Scalars["Boolean"];
+    addRole: Scalars["Boolean"];
     createMember?: Maybe<Member>;
-    incMemberDiscordActivityChannel: Scalars['Boolean'];
-    incMemberDiscordVoiceMinute: Scalars['Boolean'];
-    removeChannel: Scalars['Boolean'];
-    removeRole: Scalars['Boolean'];
-    updateMember: Scalars['Boolean'];
-    updateServerActivity: Scalars['Boolean'];
+    incMemberDiscordActivityChannel: Scalars["Boolean"];
+    incMemberDiscordVoiceMinute: Scalars["Boolean"];
+    removeChannel: Scalars["Boolean"];
+    removeRole: Scalars["Boolean"];
+    setServerActivityMemberCount: Scalars["Boolean"];
+    updateMember: Scalars["Boolean"];
 };
-
 
 export type MutationAddChannelArgs = {
-    category: Scalars['String'];
-    channelId: Scalars['ID'];
+    category: Scalars["String"];
+    channelId: Scalars["ID"];
 };
-
 
 export type MutationAddRoleArgs = {
-    category: Scalars['String'];
-    roleId: Scalars['ID'];
+    category: Scalars["String"];
+    roleId: Scalars["ID"];
 };
-
 
 export type MutationCreateMemberArgs = {
-    id: Scalars['ID'];
-    isOnServer?: InputMaybe<Scalars['Boolean']>;
-    profilPicture: Scalars['String'];
-    username: Scalars['String'];
+    id: Scalars["ID"];
+    isOnServer?: InputMaybe<Scalars["Boolean"]>;
+    profilPicture: Scalars["String"];
+    username: Scalars["String"];
 };
-
 
 export type MutationIncMemberDiscordActivityChannelArgs = {
-    channelId: Scalars['ID'];
-    id: Scalars['ID'];
+    channelId: Scalars["ID"];
+    id: Scalars["ID"];
 };
-
 
 export type MutationIncMemberDiscordVoiceMinuteArgs = {
-    id: Scalars['ID'];
+    id: Scalars["ID"];
 };
-
 
 export type MutationRemoveChannelArgs = {
-    channelId: Scalars['ID'];
+    channelId: Scalars["ID"];
 };
-
 
 export type MutationRemoveRoleArgs = {
-    roleId: Scalars['ID'];
+    roleId: Scalars["ID"];
 };
 
+export type MutationSetServerActivityMemberCountArgs = {
+    count: Scalars["Int"];
+};
 
 export type MutationUpdateMemberArgs = {
-  id: Scalars['ID'];
-  input: MemberInput;
-};
-
-
-export type MutationUpdateServerActivityArgs = {
-  input: ServerActivityInput;
+    id: Scalars["ID"];
+    input: MemberInput;
 };
 
 export type Query = {
-  __typename?: 'Query';
-  channels?: Maybe<Array<Maybe<MainChannel>>>;
-  member?: Maybe<Member>;
-  members: Array<Maybe<Member>>;
-  roles?: Maybe<Array<Maybe<MainRole>>>;
-  serverActivity: Array<Maybe<ServerActivity>>;
-  todayServerActivity: ServerActivity;
+    __typename?: "Query";
+    channels?: Maybe<Array<Maybe<MainChannel>>>;
+    member?: Maybe<Member>;
+    members: Array<Maybe<Member>>;
+    roles?: Maybe<Array<Maybe<MainRole>>>;
+    serverActivity: Array<Maybe<ServerActivity>>;
+    todayServerActivity: ServerActivity;
 };
-
 
 export type QueryMemberArgs = {
-  id: Scalars['ID'];
+    id: Scalars["ID"];
 };
 
-
 export type QueryServerActivityArgs = {
-  historyCount: Scalars['Int'];
+    historyCount: Scalars["Int"];
 };
 
 export type ServerActivity = {
-  __typename?: 'ServerActivity';
-  date: Scalars['Date'];
-  memberCount: Scalars['Int'];
-  messageCount: Scalars['Int'];
-  voiceMinute: Scalars['Int'];
-};
-
-export type ServerActivityInput = {
-  date: Scalars['Date'];
-  memberCount?: InputMaybe<Scalars['Int']>;
-  messageCount?: InputMaybe<Scalars['Int']>;
-  voiceMinute?: InputMaybe<Scalars['Int']>;
+    __typename?: "ServerActivity";
+    date: Scalars["Date"];
+    memberCount: Scalars["Int"];
+    messageCount: Scalars["Int"];
+    voiceMinute: Scalars["Int"];
 };

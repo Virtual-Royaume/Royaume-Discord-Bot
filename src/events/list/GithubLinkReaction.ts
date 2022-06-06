e1b0c278598f0f4054b8cbc2c9a1f88f8f36726f
Bluzzi <camilledugas.pro@gmail.com>
2022-06-06 18:28:59 +0200

    GithubLinkReaction (event): improvement

--- src/events/list/GithubLinkReaction.ts
+++ src/events/list/GithubLinkReaction.ts
@@ -1,59 +1,105 @@
 import { Message } from "discord.js";
-import fetch from "node-fetch";
 import Event, { EventName } from "../Event";
-import githubToken from "../../../resources/config/secret.json";
+import { githubToken } from "../../../resources/config/secret.json";
+import { githubRaw, textFetch } from "../../utils/Fetch";
 
 export default class GithubLinkReaction extends Event {
 
     public name: EventName = "messageCreate";
 
-    private regexLinkIdentification = /http(s?):\/\/(www\.)?github.com\/([^\s]+)blob([^\s]+)#L\d*/i;
-    private rawGithubDomainName = "https://raw.githubusercontent.com/";
-
     public async execute(message: Message) : Promise<void> {
+        // Get urls :
+        const urls = message.content.match(/http(s?):\/\/(www\.)?github.com\/([^\s]+)blob([^\s]+)#L\d+(-L\d+)?/gi);
 
-        const url = message.content.match(this.regexLinkIdentification)?.shift();
+        if(!urls) return;
 
-        if(!url) return;
+        // Get codes, lines... :
+        interface CodeElement {
+            link: string;
 
-        const lineNum = Number(url.match(/(\d*)$/i)?.shift());
-        if(Number.isNaN(lineNum)) return;
+            lines: number[];
+            mainLine?: number;
 
-        // Remove `https://www.github.com/` and line identification from link
-        let filePath = url.replace(/^(http(s?):\/\/(www\.)?github.com\/)|(#L\d*)$/ig, "").split("/");
-        filePath.splice(2, 1); // Remove `blob` from link
-        const fileExtension = filePath[filePath.length-1].match(/(?<=[.])\w*/gm)?.shift() ?? "";
-
-        const fileContent = (await (await fetch(
-            this.rawGithubDomainName + filePath.join("/") ,
-            {
-                headers: {
-                    Authorization: `token ${githubToken}`
-                }
-            }
-        )).text()).split("\n");
-
-        if(fileContent.shift() === '404: Not Found') return;
-
-        let selectCode = "";
-        for(let i = -5; i <= 5; i++){
-
-            const currentLineNum = lineNum+i;
-
-            if(fileContent[currentLineNum-1] === undefined) continue;
-
-            const formatedLineLune = currentLineNum === lineNum ? `>${currentLineNum} ` : ` ${currentLineNum} `;
-
-            selectCode += formatedLineLune + fileContent[currentLineNum-1] + "\n";
+            code: string;
+            fileExtension: string;
         }
 
-        message.suppressEmbeds(true); // Remove link integration
+        const codeElements: CodeElement[] = [];
+    
+        for(let url of urls){
+            // Get lines :
+            const linesNumber = [...url.matchAll(/L\d+/gi)].map(number => Number(number[0].slice(1)));
 
-        message.reply({
-            content: `> Ligne ${lineNum} de \`${filePath.join("/")}\`\n\`\`\`${fileExtension}\n${selectCode}\`\`\``,
-            allowedMentions: {
-                repliedUser: false
+            let mainLine: number | null = null;
+
+            if(linesNumber.length > 2){
+                return;
+            } else if(linesNumber.length === 2 && linesNumber[1] - linesNumber[0] > 10){
+                linesNumber[1] = linesNumber[0] + 10;
+            } else if(linesNumber.length === 1){
+                mainLine = linesNumber[0];
+
+                linesNumber[0] = linesNumber[0] - 5;
+
+                linesNumber.push(linesNumber[0] + 10);
             }
-        });
+
+            // Remove `https://www.github.com/` and line identification from link :
+            let filePath = url.replace(/^(http(s?):\/\/(www\.)?github.com\/)|(#L\d*)(-L\d*)?$/ig, "").split("/");
+            
+            // Remove "blob" from link :
+            filePath.splice(2, 1);
+
+            // Get file extension for code highlight :
+            const fileExtension = filePath[filePath.length-1].match(/(?<=[.])\w*/gm)?.shift() ?? "";
+
+            // Request for get the code :
+            const requestAuth = githubToken ? { headers: { Authorization: `token ${githubToken}` } } : {};
+            const request = await textFetch(githubRaw + filePath.join("/"), requestAuth);
+
+            if(request.status !== 200) return;
+
+            const fileContent = request.body.split("\n");
+
+            // Check if the line number is not too high :
+            if(linesNumber[linesNumber.length - 1] > fileContent.length) return;
+
+            // Format selected code :
+            let selectedCode = "";
+
+            for(let i = linesNumber[0]; i <= linesNumber[1]; i++){
+                selectedCode += `${mainLine === i ? ">" + i : " " + i} ${fileContent[i - 1]}\n`;
+            }
+
+            // Push informations :
+            const codeElement: CodeElement = {
+                link: filePath.join("/"),
+                lines: linesNumber,
+                code: selectedCode,
+                fileExtension
+            };
+
+            if(mainLine) codeElement.mainLine = mainLine;
+
+            codeElements.push(codeElement);
+        }
+
+        if(codeElements.length){
+            // Remove link integration :
+            message.suppressEmbeds(true);
+
+            // Send code messages :
+            for(let element of codeElements){
+                const codeBlock = `\`\`\`${element.fileExtension}\n${element.code}\n\`\`\``;
+                const lines = element.mainLine ? `Ligne ${element.mainLine}` : `Lignes ${element.lines.join("-")}`
+
+                message.reply({
+                    content: `> ${lines} de \`${element.link}\`\n${codeBlock}`,
+                    allowedMentions: {
+                        repliedUser: false
+                    }        
+                });
+            }
+        }
     }
 }

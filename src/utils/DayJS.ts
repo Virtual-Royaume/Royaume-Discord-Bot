69400249d1492e4314a0be23ef7cbae1e5a57b6e
Bluzzi <camilledugas.pro@gmail.com>
2022-06-14 15:05:28 +0200

    BirthdaysChecker (task): fix timezone

---
+++ src/utils/DayJS.ts
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault("Europe/Paris");
+
+const DayJS = dayjs;
+
+export default DayJS;

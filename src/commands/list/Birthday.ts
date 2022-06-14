3b630f1a56d43fc1527525c9c8096c60c740fbdc
Verre2OuiSki <monjalethan34@gmail.com>
2022-06-14 22:39:40 +0200

    Ad birthday list

--- src/commands/list/Birthday.ts
+++ src/commands/list/Birthday.ts
@@ -1,60 +1,109 @@
-import { SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builders";
+import { SlashCommandBuilder, SlashCommandNumberOption, SlashCommandStringOption, SlashCommandSubcommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 import { request } from "../../api/Request";
-import { setBirthday } from "../../api/requests/Member";
+import { getBirthdays, GetBirthdaysType, setBirthday } from "../../api/requests/Member";
 import { simpleEmbed } from "../../utils/Embed";
 import Command from "../Command";
 import { minimumAge } from "../../../resources/config/information.json";
-import { getAge } from "../../utils/Functions";
+import { dateFormat, getAge } from "../../utils/Functions";
 
 export default class Birthday extends Command {
 
     public readonly slashCommand = new SlashCommandBuilder()
         .setName("anniversaire")
         .setDescription("Définir votre date d'anniversaire")
-        .addStringOption(new SlashCommandStringOption()
-            .setName("date")
-            .setDescription("Votre date de naissance avec ce format : DD/MM/YYYY (jour/mois/année de naissance)")
-            .setRequired(true));
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName("set")
+            .setDescription("Définir votre date d'anniversaire")
+            .addStringOption(new SlashCommandStringOption()
+                .setName("date")
+                .setDescription("Votre date de naissance avec ce format : DD/MM/YYYY (jour/mois/année de naissance)")
+                .setRequired(true)))
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName("list")
+            .setDescription("Afficher la liste des anniversaires")
+            .addNumberOption(new SlashCommandNumberOption()
+                .setName("page")
+                .setDescription("Page de la liste")
+                .setMinValue(1)));
 
     public readonly defaultPermission: boolean = true;
 
-    public async execute(command: CommandInteraction) : Promise<void> {
-        // Check parameter :
-        const badFormat = (error: string) => {
-            command.reply({ embeds: [simpleEmbed(error, "error")], ephemeral: true });
-        };
+    private memberPerPage = 10;
 
-        let dateParams: number[];
+    public async execute(command: CommandInteraction): Promise<void> {
 
-        try {
-            dateParams = command.options.getString("date", true).split("/").map(element => parseInt(element));
-        } catch {
-            badFormat("Le format de votre date n'est pas bon.");
-            return;
+        switch (command.options.getSubcommand()) {
+            case "set": {
+                // Check parameter :
+                const badFormat = (error: string) => {
+                    command.reply({ embeds: [simpleEmbed(error, "error")], ephemeral: true });
+                };
+
+                let dateParams: number[];
+
+                try {
+                    dateParams = command.options.getString("date", true).split("/").map(element => parseInt(element));
+                } catch {
+                    badFormat("Le format de votre date n'est pas bon.");
+                    return;
+                }
+
+                if (dateParams.length < 3) {
+                    badFormat("La date est incomplète.");
+                    return;
+                }
+
+                // Try to parse the date :
+                const date = new Date(`${dateParams[2]}-${dateParams[1]}-${dateParams[0]}Z`);
+
+                if (isNaN(date.getTime())) {
+                    badFormat("Cette date est invalide.");
+                    return;
+                }
+
+                if (getAge(date) < minimumAge) {
+                    badFormat(`Vous devez être né il y a minimum ${minimumAge} ans`);
+                    return;
+                }
+
+                // Save birthday :
+                await request(setBirthday, { id: command.user.id, date: date.getTime() });
+
+                command.reply({ embeds: [simpleEmbed("Votre date d'anniversaire a bien été enregistrée.")], ephemeral: true });
+                break;
+            }
+            case "list": {
+                let page = command.options.getNumber("page") ?? 1;
+                let birthdays = (await request<GetBirthdaysType>(getBirthdays)).members.filter(member => member.birthday)
+                    .sort((a, b) => (a.birthday ?? 0) - (b.birthday ?? 0));
+
+                const maxPage = Math.ceil(birthdays.length / this.memberPerPage);
+                page = page > maxPage ? maxPage : page;
+
+                // Slice the members with page and max page :
+                birthdays = birthdays.slice(page * this.memberPerPage - this.memberPerPage, page * this.memberPerPage);
+
+                let message = "";
+                for (let i = 0; i < birthdays.length; i++) {
+                    const member = birthdays[i];
+                    const birthday = new Date(member.birthday ?? 0);
+                    const position = i + 1 + (page - 1) * this.memberPerPage;
+                    message += `**${position}. ${member.username} :** ${getAge(birthday)} ans (${dateFormat(new Date(birthday), "/")})\n`;
+                }
+
+                // Send leaderboard :
+                command.reply({
+                    embeds: [
+                        simpleEmbed(message, "normal", "Anniversaires des membres")
+                        // .setFooter({
+                        //     text: `• Page ${page}/${maxPage}`,
+                        // })
+                    ]
+                });
+
+                break;
+            }
         }
-
-        if (dateParams.length < 3) {
-            badFormat("La date est incomplète.");
-            return;
-        }
-
-        // Try to parse the date :
-        const date = new Date(`${dateParams[2]}-${dateParams[1]}-${dateParams[0]}Z`);
-
-        if (isNaN(date.getTime())) {
-            badFormat("Cette date est invalide.");
-            return;
-        }
-
-        if (getAge(date) < minimumAge) {
-            badFormat(`Vous devez être né il y a minimum ${minimumAge} ans`);
-            return;
-        }
-
-        // Save birthday :
-        await request(setBirthday, { id: command.user.id, date: date.getTime() });
-
-        command.reply({ embeds: [simpleEmbed("Votre date d'anniversaire a bien été enregistrée.")], ephemeral: true });
     }
 }

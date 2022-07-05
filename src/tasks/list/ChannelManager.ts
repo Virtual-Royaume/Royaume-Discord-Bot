314fe0ac526795607cadd7d9ff53bdd66d5ce823
Gaëtan H <steellgold0@gmail.com>
2022-07-05 22:27:56 +0200

    Meilleur  code
    Co-Authored-By: Bluzzi <camilledugas.pro@gmail.com>

--- src/tasks/list/ChannelManager.ts
+++ src/tasks/list/ChannelManager.ts
@@ -1,16 +1,10 @@
 import Client from "../../Client";
 import Task from "../Task";
-import { generalChannel } from "../../../resources/config/information.json";
-import { BaseGuildTextChannel } from "discord.js";
-import { simpleEmbed } from "../../utils/Embed";
 
-const defaults = [
+const channelsName = [
     "salon bleu",
     "salon rouge",
-    "salon jaune"
-];
-
-const supplementaries = [
+    "salon jaune",
     "salon vert",
     "salon violet",
     "salon orange",
@@ -20,10 +14,9 @@ const supplementaries = [
     "salon noir",
     "salon gris",
     "salon blanc",
-    "salon brun"
+    "salon brun",
+    "salon marron"
 ];
-
-const full = 1;
 
 export default class ChannelManager extends Task {
 
@@ -32,55 +25,44 @@ export default class ChannelManager extends Task {
     }
 
     public async run() : Promise<void> {
+        this.create();
+        this.delete();
+    }
+
+    private async create() : Promise<void> {
         const guild = await Client.instance.getGuild();
 
-        let countTotal = 0;
+        const channels = guild.channels.cache.filter(channel => {
+            return channelsName.includes(channel.name)
+                && channel.type === "GUILD_VOICE"
+                && channel.members.size === 0;
+        });
 
-        // Ici, on récupère les salons du serveur
-        for (const [id, channel] of guild.channels.cache.filter(channel => channel.type === "GUILD_VOICE")) {
-            // On vérifie si c'est un salon vocal
-            if (channel.type !== "GUILD_VOICE") return;
+        if (channels.size === 0) {
+            const channelToCreate = channelsName.filter(name => {
+                return !guild.channels.cache.find(channel => channel.name === name);
+            }).shift();
 
-            if (defaults.includes(channel.name)) {
-                // On vérifie si les salons ont des membres dedans
-                if (channel.members.size >= full) {
-                    countTotal++;
-                }
-
-                // On vérifie si le nombre de salons pleins est égal à la taille du tableau des salons actuels
-                if (countTotal == defaults.length) {
-                    if (supplementaries.length == 0) {
-                        // Envoyer un message dans le salon général
-                        const generalChannelInstance = await guild.channels.fetch(generalChannel);
-                        if (!(generalChannelInstance instanceof BaseGuildTextChannel)) return;
-
-                        const message = "Il n'y à plus de salons vocaux disponibles, patientez que des membres en libère un.";
-                        generalChannelInstance.send({
-                            embeds: [simpleEmbed(message, "normal", "Salons vocaux pleins")]
-                        });
-                        return;
-                    }
-
-                    // On récupère le prochain salon supplémentaire
-                    const newChannel: string = supplementaries[0];
-
-                    // On récupère le salon (de couleur) le plus bas dans le serveur
-                    const lastChannel = guild.channels.cache.find(channel => channel.name == defaults[2]);
-
-                    // On envoi le nouveau serveur dans le tableau des salons actuels
-                    defaults.push(newChannel);
-
-                    // On retire le salon supplémentaire du tableau des salons supplémentaires
-                    supplementaries.shift();
-
-                    // On crée le salon supplémentaire
-                    guild.channels.create(newChannel, {
-                        type: "GUILD_VOICE",
-                        parent: lastChannel?.parentId,
-                        position: lastChannel?.position
-                    });
-                }
+            if (channelToCreate) {
+                guild.channels.create(channelToCreate, {
+                    type: "GUILD_VOICE",
+                    parent: "853314658789490709",
+                    position: 3
+                });
             }
+        }
+    }
+
+    private async delete() : Promise<void> {
+        const channels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
+            return !channelsName.slice(0, 3).includes(channel.name)
+                && channelsName.includes(channel.name)
+                && channel.type === "GUILD_VOICE"
+                && channel.members.size === 0;
+        }).values();
+
+        for (const channel of channels) {
+            channel.delete();
         }
     }
 }

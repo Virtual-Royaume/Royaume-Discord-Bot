0c36745f3e15451b33cba4db38d257136bb484e3
Gaëtan H <steellgold0@gmail.com>
2022-07-06 22:30:22 +0200

    Déplacement vers la configuration

--- src/tasks/list/ChannelManager.ts
+++ src/tasks/list/ChannelManager.ts
@@ -1,22 +1,7 @@
 import Client from "../../Client";
 import Task from "../Task";
 
-const channelsName = [
-    "salon bleu",
-    "salon rouge",
-    "salon jaune",
-    "salon vert",
-    "salon violet",
-    "salon orange",
-    "salon rose",
-    "salon turquoise",
-    "salon rose",
-    "salon noir",
-    "salon gris",
-    "salon blanc",
-    "salon brun",
-    "salon marron"
-];
+import { voiceChannels } from "../../../resources/config/information.json";
 
 export default class ChannelManager extends Task {
 
@@ -27,77 +12,67 @@ export default class ChannelManager extends Task {
     public async run() : Promise<void> {
         this.create();
         this.delete();
-        this.createPrivates();
-        this.deletePrivates();
     }
 
     private async create() : Promise<void> {
+        // PUBLIC
         const guild = await Client.instance.getGuild();
 
         const channels = guild.channels.cache.filter(channel => {
-            return channelsName.includes(channel.name)
+            return voiceChannels.channelsNames.includes(channel.name)
                 && channel.type === "GUILD_VOICE"
                 && channel.members.size === 0;
         });
 
         if (channels.size === 0) {
-            const channelToCreate = channelsName.filter(name => {
+            const channelToCreate = voiceChannels.channelsNames.filter(name => {
                 return !guild.channels.cache.find(channel => channel.name === name);
             }).shift();
 
             if (channelToCreate) {
                 guild.channels.create(channelToCreate, {
                     type: "GUILD_VOICE",
-                    parent: "853314658789490709",
-                    position: 3
+                    parent: voiceChannels.category,
+                    position: voiceChannels.defaultPosition
                 });
             }
         }
-    }
 
-    private async delete() : Promise<void> {
-        const channels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
-            return !channelsName.slice(0, 3).includes(channel.name)
-                && channelsName.includes(channel.name)
-                && channel.type === "GUILD_VOICE"
-                && channel.members.size === 0;
-        }).values();
-
-        for (const channel of channels) {
-            channel.delete();
-        }
-    }
-
-    // PRIVATE
-
-    private async createPrivates() : Promise<void> {
-        const guild = await Client.instance.getGuild();
-
-        const channels = guild.channels.cache.filter(channel => {
+        // PRIVATE
+        const privateChannels = guild.channels.cache.filter(channel => {
             return channel.name == "salon privé"
                 && channel.type === "GUILD_VOICE"
                 && channel.members.size === 0;
         });
 
-        if (channels.size === 0) {
-            guild.channels.create("salon privé", {
+        if (privateChannels.size === 0) {
+            guild.channels.create(voiceChannels.privateChannel.name, {
                 type: "GUILD_VOICE",
-                parent: "853314658789490709",
-                position: 4,
-                userLimit: 2
+                parent: voiceChannels.category,
+                position: voiceChannels.privateChannel.position,
+                userLimit: voiceChannels.privateChannel.userLimit
             });
         }
     }
 
-    private async deletePrivates() : Promise<void> {
+    private async delete() : Promise<void> {
+        // PUBLIC
         const channels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
-            return channel.name == "salon privé"
+            return !voiceChannels.channelsNames.slice(0, 3).includes(channel.name)
+                && voiceChannels.channelsNames.includes(channel.name)
                 && channel.type === "GUILD_VOICE"
                 && channel.members.size === 0;
         }).values();
 
-        for (const channel of channels) {
-            channel.delete();
-        }
+        for (const channel of channels) channel.delete();
+
+        // PRIVATE
+        const privateChannels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
+            return channel.name == voiceChannels.privateChannel.name
+                && channel.type === "GUILD_VOICE"
+                && channel.members.size === 0;
+        }).values();
+
+        for (const privateChannel of privateChannels) privateChannel.delete();
     }
 }

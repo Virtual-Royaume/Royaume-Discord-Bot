c7e4fdd1206d0af405b9d62713bd05db41b1a493
Bluzzi <camilledugas.pro@gmail.com>
2022-07-07 04:34:10 +0200

    ChannelManager (task): Clean/optimization and finish the TODO list

--- src/tasks/list/ChannelManager.ts
+++ src/tasks/list/ChannelManager.ts
@@ -1,78 +1,83 @@
 import Client from "../../Client";
 import Task from "../Task";
-
 import { voiceChannels } from "../../../resources/config/information.json";
+import { Collection, VoiceChannel } from "discord.js";
+
+type ChannelType = "public" | "private";
 
 export default class ChannelManager extends Task {
 
     constructor() {
         super(60_000);
+
+        // TODO : createDefaultChannels()
     }
 
     public async run() : Promise<void> {
-        this.create();
-        this.delete();
-    }
+        // Creation of new voice channels if they are all full :
+        if (!(await this.getEmptyChannels(voiceChannels.public.nameList)).size) {
+            const channelToCreate = await this.getAvailablePublicChannel();
 
-    private async create() : Promise<void> {
-        // PUBLIC
-        const guild = await Client.instance.getGuild();
-
-        const channels = guild.channels.cache.filter(channel => {
-            return voiceChannels.channelsNames.includes(channel.name)
-                && channel.type === "GUILD_VOICE"
-                && !channel.members.size;
-        });
-
-        if (channels.size === 0) {
-            const channelToCreate = voiceChannels.channelsNames.filter(name => {
-                return !guild.channels.cache.find(channel => channel.name === name);
-            }).shift();
-
-            if (channelToCreate) {
-                guild.channels.create(channelToCreate, {
-                    type: "GUILD_VOICE",
-                    parent: voiceChannels.category,
-                    position: voiceChannels.defaultPosition
-                });
-            }
+            if (channelToCreate) this.createChannel(channelToCreate, "public");
         }
 
-        // PRIVATE
-        const privateChannels = guild.channels.cache.filter(channel => {
-            return channel.name == "salon privé"
-                && channel.type === "GUILD_VOICE"
-                && !channel.members.size;
-        });
-
-        if (privateChannels.size === 0) {
-            guild.channels.create(voiceChannels.privateChannel.name, {
-                type: "GUILD_VOICE",
-                parent: voiceChannels.category,
-                position: voiceChannels.privateChannel.position,
-                userLimit: voiceChannels.privateChannel.userLimit
-            });
+        if (!(await this.getEmptyChannels(voiceChannels.private.name)).size) {
+            this.createChannel(voiceChannels.private.name, "private");
         }
+
+        // Deletion of unoccupied extra rooms :
+        const channels = Array.from(
+            (await this.getEmptyChannels(voiceChannels.public.nameList.slice(voiceChannels.public.defaultCount))).values()
+        ).splice(-1);
+        const privateChannels = Array.from((await this.getEmptyChannels(voiceChannels.private.name)).values()).splice(-1);
+
+        for (const channel of [...channels, ...privateChannels]) channel.delete();
     }
 
-    private async delete() : Promise<void> {
-        // PUBLIC
-        const channels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
-            return !voiceChannels.channelsNames.slice(0, voiceChannels.defaultPublics).includes(channel.name)
-                && voiceChannels.channelsNames.includes(channel.name)
-                && channel.type === "GUILD_VOICE"
-                && !channel.members.size;
-        }).values();
+    /**
+     * Create a voice channel in the category defined in the configuration.
+     * Define its number of places and its position according to its type (public or private).
+     */
+    private async createChannel(name: string, type: ChannelType) : Promise<void> {
+        const channelPosition = await this.getChannelCount("public") + 1;
 
-        for (const channel of channels) channel.delete();
+        (await Client.instance.getGuild()).channels.create(name, {
+            type: "GUILD_VOICE",
+            parent: voiceChannels.category,
+            position: type === "public" ? channelPosition : channelPosition + await this.getChannelCount("private"),
+            userLimit: type === "private" ? 2 : undefined
+        });
+    }
 
-        // PRIVATE
-        const privateChannels = (await Client.instance.getGuild()).channels.cache.filter(channel => {
-            return channel.name == voiceChannels.privateChannel.name
-                && channel.type === "GUILD_VOICE"
-                && !channel.members.size;
-        }).values();
+    /**
+     * This function allows you to obtain a list of voice channel that respects the desired name(s) and that does not contain any member.
+     * @param channelsNames desired name(s)
+     */
+    private async getEmptyChannels(channelsNames: string | string[]) : Promise<Collection<string, VoiceChannel>> {
+        return (await Client.instance.getGuild()).channels.cache.filter((channel): channel is VoiceChannel => {
+            return channel.type === "GUILD_VOICE"
+                && channel.parentId === voiceChannels.category
+                && !channel.members.size
+                && channelsNames.includes(channel.name);
+        });
+    }
 
-        for (const privateChannel of privateChannels) privateChannel.delete();
+    /**
+     * Returns a public voice channel name if there is still one available (not used) in the list defined in the configuration.
+     */
+    private async getAvailablePublicChannel() : Promise<string | undefined> {
+        return voiceChannels.public.nameList.filter(async name => {
+            return !(await Client.instance.getGuild()).channels.cache.find(channel => channel.name === name);
+        }).shift();
+    }
+
+    private async getChannelCount(channelType: ChannelType) : Promise<number> {
+        const channelsNames = channelType === "public" ? voiceChannels.public.nameList : voiceChannels.private.name;
+
+        return (await Client.instance.getGuild()).channels.cache.filter(channel => {
+            return channel.type === "GUILD_VOICE"
+                && channel.parentId === voiceChannels.category
+                && channelsNames.includes(channel.name)
+        }).size;
     }
 }

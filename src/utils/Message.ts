165882b87d99c5e870c5b15eb9610625daba8d37
Gaëtan H <steellgold0@gmail.com>
2022-08-15 19:02:56 +0200

    Valeur par défaut à params

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -1,6 +1,6 @@
 import messages from "../../resources/config/messages.json";
 
-export function message(key: string, params: any[]): string {
+export function message(key: string, params: any[] = []): string {
     const messageList: Record<string, string> = <Record<string, string>>messages;
     let message = messageList[key];
     if (!message) return "Aucun message trouvé (" + key + ")";

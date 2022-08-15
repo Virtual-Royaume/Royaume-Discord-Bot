d7d05407212afd77663bea6fa597c8a14db85d33
Gaëtan H <steellgold0@gmail.com>
2022-08-15 18:57:16 +0200

    Remplace les mots entres {} par les arguments

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -2,12 +2,14 @@ import messages from "../../resources/config/messages.json";
 
 export function message(key: string, params: any[]): string {
     const messageList: Record<string, string> = <Record<string, string>>messages;
-    const message = messageList[key];
-
-    // On vérifie si la clé "key" existe dans "messages.json"
+    let message = messageList[key];
     if (!message) return "Aucun message trouvé (" + key + ")";
 
-    // Les paramètres sont par positions donc "Bonjour {member} tu a {age}"
-    // => params[0] = member, params[1] = age
-    return message.replace(/\{(\d+)\}/g, (match, index) => params[index]);
+    const words = message.match(/\{[^}]+\}/g);
+    if (words) {
+        for (let i = 0; i < words.length; i++) {
+            message = message.replace(words[i], params[i]);
+        }
+    }
+    return message;
 }

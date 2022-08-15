05599fc13fc5814cd4833930795b4c9719dc4955
Gaëtan H <steellgold0@gmail.com>
2022-08-15 18:21:13 +0200

    Pas d'erreur en mode dev :sunglasses:

--- src/events/list/GuildMemberAdd.ts
+++ src/events/list/GuildMemberAdd.ts
@@ -11,19 +11,20 @@ export default class GuildMemberAdd extends Event {
 
     public async execute(member: GuildMember) : Promise<void> {
         if (member.user.bot) return;
+        if (!Client.instance.isProdEnvironment) {
+            // Add verification role :
+            const role = await (await Client.instance.getGuild()).roles.fetch(verify.roles.waiting);
 
-        // Add verification role :
-        const role = await (await Client.instance.getGuild()).roles.fetch(verify.roles.waiting);
+            if (role) member.roles.add(role);
 
-        if (role) member.roles.add(role);
+            // Create the member if he dosen't exist :
+            const result = await request<CreateMemberType>(createMember, {
+                id: member.id,
+                username: member.user.username,
+                profilePicture: member.user.avatarURL() ?? "https://i.ytimg.com/vi/Ug9Xh-xNecM/maxresdefault.jpg"
+            });
 
-        // Create the member if he dosen't exist :
-        const result = await request<CreateMemberType>(createMember, {
-            id: member.id,
-            username: member.user.username,
-            profilePicture: member.user.avatarURL() ?? "https://i.ytimg.com/vi/Ug9Xh-xNecM/maxresdefault.jpg"
-        });
-
-        if (!result.createMember._id) request(setAlwaysOnServer, { id: member.id, value: true });
+            if (!result.createMember._id) request(setAlwaysOnServer, { id: member.id, value: true });
+        }
     }
 }

91f5ef291374a6481a7d2d341322c72d004b3df4
Gaëtan H <steellgold0@gmail.com>
2022-08-17 15:51:18 +0200

    Inactive :white_check_mark:

--- src/commands/list/Inactive.ts
+++ src/commands/list/Inactive.ts
@@ -3,13 +3,14 @@ import { CommandInteraction } from "discord.js";
 import { request } from "../../api/Request";
 import { getMonthActivity, GetMonthActivityType } from "../../api/requests/Member";
 import { simpleEmbed } from "../../utils/Embed";
+import { msg } from "../../utils/Message";
 import Command from "../Command";
 
 export default class Inactive extends Command {
 
     public readonly slashCommand = new SlashCommandBuilder()
-        .setName("inactive")
-        .setDescription("Voir les membres inactifs ce mois-ci");
+        .setName(msg("cmd-inactive-builder-name"))
+        .setDescription(msg("cmd-inactive-builder-description"));
 
     public async execute(command: CommandInteraction) : Promise<void> {
         const members = (await request<GetMonthActivityType>(getMonthActivity))
@@ -23,9 +24,9 @@ export default class Inactive extends Command {
         if (members.length) {
             const message = members.map(member => "<@" + member._id + ">").join(", ");
 
-            command.reply({ embeds: [simpleEmbed(message, "normal", "Liste des membres inactif")], ephemeral: true });
+            command.reply({ embeds: [simpleEmbed(message, "normal", msg("cmd-inactive-exec-inactives"))], ephemeral: true });
         } else {
-            command.reply({ embeds: [simpleEmbed("Aucun membre n'est inactif.", "error")], ephemeral: true });
+            command.reply({ embeds: [simpleEmbed(msg("cmd-inactive-exec-no-inactives"), "error")], ephemeral: true });
         }
     }
 }

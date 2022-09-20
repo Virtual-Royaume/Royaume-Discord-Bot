4a715294ee1d05c93583b654f148e4388218229a
Bluzzi <camilledugas.pro@gmail.com>
2022-09-21 00:16:32 +0200

    Embed (util): migration to DJS V14

--- src/utils/Embed.ts
+++ src/utils/Embed.ts
@@ -1,10 +1,10 @@
-import { HexColorString, MessageEmbed } from "discord.js";
+import { HexColorString, EmbedBuilder } from "discord.js";
 import { colors } from "$resources/config/information.json";
 
 type EmbedType = "normal" | "error";
 
-export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string) : MessageEmbed {
-    return new MessageEmbed()
+export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string) : EmbedBuilder {
+    return new EmbedBuilder()
         .setDescription(message)
         .setColor((type === "normal" ? colors.primary : colors.error) as HexColorString)
         .setTitle(title ?? "");

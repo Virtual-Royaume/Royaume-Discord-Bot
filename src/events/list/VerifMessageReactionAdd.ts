a320f7b12476feebd3ef9e5c6dff54de422e8b46
Verre2OuiSki <verre2ouiski@gmail.com>
2022-09-30 16:50:58 +0200

    VerifMessageReactionAdd event done

--- src/events/list/VerifMessageReactionAdd.ts
+++ src/events/list/VerifMessageReactionAdd.ts
@@ -3,12 +3,13 @@ import Client from "$core/Client";
 import Event, { EventName } from "$core/events/Event";
 import { verify } from "$resources/config/information.json";
 import { simpleEmbed } from "$core/utils/Embed";
+import { msg } from "$core/utils/Message";
 
 export default class VerifMessageReactionAdd extends Event {
 
     public name: EventName = "messageReactionAdd";
 
-    public async execute(messageReaction: MessageReaction) : Promise<void> {
+    public async execute(messageReaction: MessageReaction): Promise<void> {
         // Check if the embed are a "presentation" message :
         const embed = (await messageReaction.fetch()).message.embeds[0];
 
@@ -35,12 +36,9 @@ export default class VerifMessageReactionAdd extends Event {
             await member.roles.add(verify.roles.verified);
             await member.roles.remove(verify.roles.waiting);
 
-            const embed = simpleEmbed(
-                "Les rôles que vous voyez sur votre droite sont définis selon votre activité au sein du Royaume !\n\n"
-                + "Pour peut choisir les rôles qui convient à vos centres d'intérêt avec la commande ``/role``"
-            );
+            const embed = simpleEmbed(msg("event-verifmessagereactionadd-exec-embed-member-accepted"));
 
-            messageReaction.message.reply({ content: `Bienvenue parmis nous <@${member.id}> !`, embeds: [embed] });
+            messageReaction.message.reply({ content: msg("event-verifmessagereactionadd-exec-welcome-message", [member.id]), embeds: [embed] });
 
             removeReactions();
         }
@@ -51,9 +49,9 @@ export default class VerifMessageReactionAdd extends Event {
             && messageReaction.count - 1 >= verify.reactionNeededCount.downVote
         ) {
             // Reject the member :
-            await messageReaction.message.reply({ embeds: [simpleEmbed(`La présentation de ${member.displayName} a été refusé.`, "error")] });
+            await messageReaction.message.reply({ embeds: [simpleEmbed(msg("event-verifmessagereactionadd-exec-embed-member-rejected", [member.displayName]), "error")] });
 
-            await member.send({ embeds: [simpleEmbed("Votre présentation n'a pas convaincu les membres du Royaume.", "error")] });
+            await member.send({ embeds: [simpleEmbed(msg("event-verifmessagereactionadd-exec-embed-member-rejected-mp"), "error")] });
 
             member.kick();
 

27eaba3565310ff0c9a66cde88ba347d344fa900
Verre2OuiSki <verre2ouiski@gmail.com>
2022-09-30 17:35:59 +0200

    Update "msg" function Add array config message for multiple lines messages

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -1,21 +1,24 @@
 import messages from "$resources/config/messages.json";
 
-/// Format du fichier JSON :
-// SlashCommandBuilder: "command-{Nom de la commande}-builder-{variable}"
-// Execute: "command-{Nom de la commande}-exec-{variable}"
+/*
+    Format du fichier JSON :
+    SlashCommandBuilder: "command-{Nom de la commande}-builder-{variable}"
+    Execute: "command-{Nom de la commande}-exec-{variable}"
+    Exemple: "command-together-builder-name": "watch-together"
+    Exemple: "command-together-exec-voice-needed": "Vous devez êtr[...]"
+*/
 
-// Exemple: "command-together-builder-name": "watch-together"
-// Exemple: "command-together-exec-voice-needed": "Vous devez êtr[...]"
-export function msg(key: string, params: any[] = []): string {
-    const messageList: Record<string, string> = <Record<string, string>>messages;
+export function msg(key: string, params: (string | number)[] = []): string {
+    const messageList: Record<string, string | string[]> = messages;
     let message = messageList[key];
+
     if (!message) return "Aucun message trouvé (" + key + ")";
 
+    if (Array.isArray(message)) message = message.join("\n");
+
     const words = message.match(/\{[^}]+\}/g);
-    if (words) {
-        for (let i = 0; i < words.length; i++) {
-            message = message.replace(words[i], params[i]);
-        }
-    }
+
+    if (words) for (let i = 0; i < words.length; i++) message = message.replace(words[i], String(params[i]));
+
     return message;
 }

4da69b51a35b6cb08aa0f5c8a26bd8809fd81aa4
Bluzzi <camilledugas.pro@gmail.com>
2022-10-01 23:19:07 +0200

    Add typing for GraphQL variables

--- src/api/requests/MainChannel.ts
+++ src/api/requests/MainChannel.ts
@@ -2,6 +2,10 @@ import { gql } from "graphql-request";
 import { MainChannel } from "$core/api/Schema";
 
 export type AddChannelType = { addChannel: boolean };
+export type AddChannelVariables = {
+    channelId: string;
+    category: string;
+};
 export const addChannel = gql`
     mutation($channelId: ID!, $category: String!){
         addChannel(channelId: $channelId, category: $category)
@@ -9,6 +13,9 @@ export const addChannel = gql`
 `;
 
 export type RemoveChannelType = { removeChannel: boolean };
+export type RemoveChannelVariables = {
+    channelId: string;
+}
 export const removeChannel = gql`
     mutation($channelId: ID!){
         removeChannel(channelId: $channelId)

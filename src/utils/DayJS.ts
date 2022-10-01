c283f308d436a81b73209aa158abab8f69f47cb2
Gaëtan H <steellgold0@gmail.com>
2022-10-01 22:27:52 +0000

    Noms des mois en Français

--- src/utils/DayJS.ts
+++ src/utils/DayJS.ts
@@ -1,11 +1,30 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
 import timezone from "dayjs/plugin/timezone.js";
+import UpdateLocale from "dayjs/plugin/updateLocale.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 dayjs.tz.setDefault("Europe/Paris");
+
+dayjs.extend(UpdateLocale);
+dayjs.updateLocale("en", {
+  months: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ]
+});
 
 const DayJS = dayjs;
 

f2365212c07085cdd7b33c7e48dbdf866b3e680c
Verre2OuiSki <verre2ouiski@gmail.com>
2022-10-02 16:19:29 +0200

    Close #100 done

--- src/utils/Embed.ts
+++ src/utils/Embed.ts
@@ -3,9 +3,9 @@ import { colors } from "$resources/config/information.json";
 
 type EmbedType = "normal" | "error";
 
-export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string) : EmbedBuilder {
+export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string): EmbedBuilder {
     return new EmbedBuilder()
         .setDescription(message)
         .setColor((type === "normal" ? colors.primary : colors.error) as HexColorString)
-        .setTitle(title ?? "");
+        .setTitle(title ?? null);
 }

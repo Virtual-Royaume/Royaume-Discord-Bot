88a425fd4ec73fff51b13b65b1905a5ffa17bccd
Gaëtan H <steellgold0@gmail.com>
2022-10-02 17:51:42 +0200

    Utiliser i18n :white_check_mark:

--- src/utils/DayJS.ts
+++ src/utils/DayJS.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
 import timezone from "dayjs/plugin/timezone.js";
 import UpdateLocale from "dayjs/plugin/updateLocale.js";
+import fr from "dayjs/locale/fr.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -9,22 +10,7 @@ dayjs.extend(timezone);
 dayjs.tz.setDefault("Europe/Paris");
 
 dayjs.extend(UpdateLocale);
-dayjs.updateLocale("en", {
-  months: [
-    "Janvier",
-    "Février",
-    "Mars",
-    "Avril",
-    "Mai",
-    "Juin",
-    "Juillet",
-    "Août",
-    "Septembre",
-    "Octobre",
-    "Novembre",
-    "Décembre",
-  ]
-});
+dayjs.locale(fr);
 
 const DayJS = dayjs;
 

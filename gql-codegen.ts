f2885658dffc4b67d66499e9b1f733b693d9c57e
Bluzzi <camilledugas.pro@gmail.com>
2022-10-17 07:15:40 +0200

    Create gql-codegen.ts

---
+++ gql-codegen.ts
@@ -0,0 +1,20 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import { getStringEnv } from "./src/utils/EnvVariable";
+
+const apiLink = getStringEnv("API_LINK");
+const apiToken = getStringEnv("API_TOKEN");
+
+const config: CodegenConfig = {
+    schema: [
+        { [apiLink]: { headers: { authorization: apiToken } } }
+    ],
+    documents: "./src/**/*.ts",
+    generates: {
+        "./src/utils/request/gql": {
+            preset: "client",
+            plugins: []
+        }
+    }
+};
+
+export default config;

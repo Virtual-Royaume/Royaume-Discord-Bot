6987d59936bb8661779c5e5a5c1aa3cfcda514e0
Bluzzi <camilledugas.pro@gmail.com>
2023-01-18 17:16:57 +0100

    gql-codegen: linting

--- gql-codegen.ts
+++ gql-codegen.ts
@@ -5,16 +5,16 @@ const apiLink = getStringEnv("API_LINK");
 const apiToken = getStringEnv("API_TOKEN");
 
 const config: CodegenConfig = {
-    schema: [
-        { [apiLink]: { headers: { authorization: apiToken } } }
-    ],
-    documents: "./src/**/*.ts",
-    generates: {
-        "./src/utils/request/graphql": {
-            preset: "client",
-            plugins: []
-        }
+  schema: [
+    { [apiLink]: { headers: { authorization: apiToken } } }
+  ],
+  documents: "./src/**/*.ts",
+  generates: {
+    "./src/utils/request/graphql": {
+      preset: "client",
+      plugins: []
     }
+  }
 };
 
 export default config;

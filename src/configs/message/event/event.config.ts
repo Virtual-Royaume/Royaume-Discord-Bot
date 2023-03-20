09509bc97f682ff3ff18c4d31fe7b60ecedffb02
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-20 13:37:39 +0100

    Support `roulette` event

--- src/configs/message/event/event.config.ts
+++ src/configs/message/event/event.config.ts
@@ -4,5 +4,8 @@ export const events = {
   "rolesSelector": {
     fetchError: "Erreur lors de l'acquisition des roles.",
     succes: "Modifications de vos rôles effectuées dans la categorie **{category}**."
+  },
+  "roulette": {
+    succes: "Parmis {choices}\n\nLe destin a choisi `{choice}`"
   }
 } satisfies Record<string, BaseMessage>;
---
+++ src/events/roulette/roulette.event.ts
@@ -0,0 +1,27 @@
+import { choicesInputId, titleInputId } from "$core/commands/roulette/roulette.util";
+import { interactionId } from "$core/configs";
+import { events } from "$core/configs/message/event";
+import { simpleEmbed } from "$core/utils/embed";
+import { EventExecute, EventName } from "$core/utils/handler/event";
+import { msgParams } from "$core/utils/message";
+
+export const event: EventName = "interactionCreate";
+
+export const execute: EventExecute<"interactionCreate"> = (interaction) => {
+  if (!interaction.isModalSubmit() || interaction.customId !== interactionId.modal.roulette) return;
+
+  const title = interaction.fields.getTextInputValue(titleInputId);
+  const choices = interaction.fields.getTextInputValue(choicesInputId).split("\n").map(choice => choice.trim()).filter(choice => choice.length > 0);
+  const choice = choices[Math.floor(Math.random() * choices.length)];
+
+  interaction.reply({
+    embeds: [simpleEmbed(
+      msgParams(events.roulette.succes, [
+        choices.map(c => `\`${c}\``).join(", "),
+        choice
+      ]),
+      "normal",
+      title.length < 1 ? undefined : title
+    )]
+  });
+};

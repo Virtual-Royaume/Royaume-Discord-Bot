f756da57bc34a72a0682c9ad064df71bced2bc96
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-21 11:49:10 +0100

    Remove `enableInDev` in `top-message` command

--- src/commands/top-message/top-message.builder.ts
+++ src/commands/top-message/top-message.builder.ts
@@ -1,9 +1,6 @@
 import { commands } from "$core/configs/message/command";
-import { EnableInDev } from "$core/utils/handler";
 import { SlashCommandDefition } from "$core/utils/handler/command";
 import { ChannelType, SlashCommandBuilder } from "discord.js";
-
-export const enableInDev: EnableInDev = true;
 
 export const slashCommand: SlashCommandDefition = new SlashCommandBuilder()
   .setName(commands.topMessage.name)

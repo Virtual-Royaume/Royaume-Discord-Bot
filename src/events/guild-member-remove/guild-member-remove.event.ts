3af19e218917333a88c284f2d24943fb8da7df84
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-22 14:11:17 +0100

    Support `guild-member-remove` event

--- src/events/guild-member-remove/guild-member-remove.event.ts
+++ src/events/guild-member-remove/guild-member-remove.event.ts
@@ -1,10 +1,21 @@
-import { EnableInDev } from "$core/utils/handler";
+import { setAlwaysOnServer } from "$core/api/requests/member";
+import { client } from "$core/client";
+import { guilds } from "$core/configs/guild";
 import { EventExecute, EventName } from "$core/utils/handler/event";
+import { gqlRequest } from "$core/utils/request";
 
-export const enableInDev: EnableInDev = true;
+export const event: EventName = "guildMemberRemove";
 
-export const event: EventName = "guildMemberAdd";
+export const execute: EventExecute<"guildMemberRemove"> = async(member) => {
+  if (member.user.bot) return;
 
-export const execute: EventExecute<"guildMemberAdd"> = async(member) => {
+  const pro = await client.guilds.fetch(guilds.pro.guildId);
+  const game = await client.guilds.fetch(guilds.game.guildId);
 
+  if ((await pro.members.fetch()).find(m => m.id === member.id) || (await game.members.fetch()).find(m => m.id === member.id)) return;
+
+  gqlRequest(setAlwaysOnServer, {
+    id: member.id,
+    value: false
+  });
 };

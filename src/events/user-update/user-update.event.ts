434bfeb46e0e8fcddcdb42af7b6bd4fd7c791230
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-23 10:48:27 +0100

    Remove `enableInDev` from `user-update`

--- src/events/user-update/user-update.event.ts
+++ src/events/user-update/user-update.event.ts
@@ -1,9 +1,6 @@
 import { EventExecute, EventName } from "$core/utils/handler/event";
 import { gqlRequest } from "$core/utils/request";
-import { EnableInDev } from "$core/utils/handler";
 import { setUsernameAndProfilePicture } from "$core/api/requests/member";
-
-export const enableInDev: EnableInDev = true;
 
 export const event: EventName = "userUpdate";
 

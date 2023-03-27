cf03c6ed4454184a7bb7f36a9f125bf94b9ac79b
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-27 11:30:47 +0200

    Init commit

--- src/commands/inactive/inactive.cmd.ts
+++ src/commands/inactive/inactive.cmd.ts
@@ -1,32 +1,4 @@
-import { getInactiveGuildMembers } from "$core/commands/inactive/inactive.util";
-import { simpleEmbed } from "$core/utils/embed";
 import { CommandExecute } from "$core/utils/handler/command";
-import { commands } from "$resources/config/messages.json";
 
-export const execute: CommandExecute = async(command) => {
-  if (!command.member) {
-    command.reply({
-      embeds: [simpleEmbed(commands.inactive.exec.notGuildMember, "error")],
-      ephemeral: true
-    });
-    return;
-  }
-
-  const inactiveMembers = await getInactiveGuildMembers();
-
-  if (!inactiveMembers) {
-    command.reply({
-      embeds: [simpleEmbed(commands.inactive.exec.activityQueryError, "error")],
-      ephemeral: true
-    });
-    return;
-  }
-
-  if (!inactiveMembers.length) {
-    command.reply({
-      embeds: [simpleEmbed(commands.inactive.exec.noInactiveMembers, "error")],
-      ephemeral: true
-    });
-    return;
-  }
+export const execute: CommandExecute = (command) => {
 };

b9c5cce91afe1761fc38981213f2f788600eb753
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-29 11:33:39 +0200

    Remove `enableInDev`

--- src/commands/inactive/inactive.builder.ts
+++ src/commands/inactive/inactive.builder.ts
@@ -2,9 +2,6 @@ import { SlashCommandDefition } from "$core/utils/handler/command";
 import { SlashCommandBuilder } from "discord.js";
 import { commands } from "$core/configs/message/command";
 import { GuildType } from "$core/configs/guild";
-import { EnableInDev } from "$core/utils/handler";
-
-export const enableInDev: EnableInDev = true;
 
 export const guild: GuildType = "pro";
 

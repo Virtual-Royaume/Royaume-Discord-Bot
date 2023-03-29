b2a247d0e06abb432a37343e7bfe723fff52ef49
Verre2OuiSki <verre2ouiski@gmail.com>
2023-03-29 11:31:21 +0200

    Use messages

--- src/commands/inactive/inactive.cmd.ts
+++ src/commands/inactive/inactive.cmd.ts
@@ -1,34 +1,57 @@
 import { getInactiveMembers } from "$core/api/func/member";
-import { confirmationModal, formatPage, getPage, pageNumberByMember, reasonId } from "$core/commands/inactive/inactive.util";
+import { confirmationModal, getActionRow, getEmbed, getPage, pageNumberByMember, reasonId } from "$core/commands/inactive/inactive.util";
 import { interactionId } from "$core/configs";
 import { guilds } from "$core/configs/guild";
 import { commands } from "$core/configs/message/command";
 import { simpleEmbed } from "$core/utils/embed";
 import { CommandExecute } from "$core/utils/handler/command";
 import { msgParams } from "$core/utils/message";
-import { ComponentType, GuildMember, PermissionFlagsBits } from "discord.js";
+import { CacheType, ComponentType, GuildMember, ModalSubmitInteraction, PermissionFlagsBits } from "discord.js";
 
 export const execute: CommandExecute = async(command) => {
   if (command.guild?.id !== guilds.pro.guildId) {
-    // Msg
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.notInGuild, "error")],
+      ephemeral: true
+    });
     return;
   }
 
   if (!command.channel) {
-    // Msg
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.noChannel, "error")],
+      ephemeral: true
+    });
     return;
   }
 
   const member = command.member;
+
   if (!(member instanceof GuildMember)) {
-    // Msg
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.notGuildMember, "error")],
+      ephemeral: true
+    });
     return;
   }
 
   const inactiveMembers = await getInactiveMembers();
 
+  // Query error
   if (!inactiveMembers) {
-    // MSG
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.activityQueryError, "error")],
+      ephemeral: true
+    });
+    return;
+  }
+
+  // No inactive
+  if (inactiveMembers.length < 1) {
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.noInactiveMembers, "error")],
+      ephemeral: true
+    });
     return;
   }
 
@@ -40,16 +63,18 @@ export const execute: CommandExecute = async(command) => {
   const page = await getPage(inactiveMembersOnServer, command.options.getInteger(commands.inactive.options.page.name) ?? 1);
 
   if (!page) {
-    // Msg
+    command.reply({
+      embeds: [simpleEmbed(commands.inactive.exec.memberQueryError, "error")],
+      ephemeral: true
+    });
     return;
   }
 
   pageNumberByMember.set(member.id, page.page);
 
-  const formattedPage = formatPage(page, member.permissions.has(PermissionFlagsBits.KickMembers));
   const replyMessage = await command.reply({
-    embeds: [formattedPage.embed],
-    components: [formattedPage.components],
+    embeds: [getEmbed(page)],
+    components: [getActionRow(page, member.permissions.has(PermissionFlagsBits.KickMembers))],
     ephemeral: true
   });
   const collector = replyMessage.createMessageComponentCollector<ComponentType.Button>({
@@ -59,43 +84,75 @@ export const execute: CommandExecute = async(command) => {
   collector.on("collect", async interaction => {
     const ids = interactionId.button.inactive;
     const id = interaction.customId;
-
-    let newPageNum = 1;
     const currentPage = pageNumberByMember.get(member.id) ?? 1;
+    let newPageNum = currentPage;
 
     if (id === ids.inactiveKick) {
       if (!member.permissions.has(PermissionFlagsBits.KickMembers)) {
-        // msg
+        interaction.reply({
+          embeds: [simpleEmbed(commands.inactive.exec.kick.noPermission, "error")],
+          ephemeral: true
+        });
+        return;
+      }
+
+      const inactiveMember = inactiveMembersOnServer[currentPage - 1];
+
+      if (!inactiveMember.kickable) {
+        interaction.reply({
+          embeds: [simpleEmbed(commands.inactive.exec.kick.botCantKick, "error")],
+          ephemeral: true
+        });
         return;
       }
 
       try {
-        await interaction.showModal(confirmationModal);
+        await interaction.showModal(confirmationModal(inactiveMember.user.username));
+        let modalSubmit: ModalSubmitInteraction<CacheType>;
 
-        const modalSubmit = await interaction.awaitModalSubmit({
-          filter: (interaction) => interaction.customId === interactionId.modal.inactive && interaction.user.id === member.id,
-          time: 1000 * 60 * 5
-        });
+        try {
+          modalSubmit = await interaction.awaitModalSubmit({
+            filter: (interaction) => interaction.customId === interactionId.modal.inactive && interaction.user.id === member.id,
+            time: 1000 * 60 * 5
+          });
+
+          // Modal time expires
+        } catch (error) {
+          interaction.followUp({
+            embeds: [simpleEmbed(commands.inactive.exec.kick.kickTimeExpired, "error")],
+            ephemeral: true
+          });
+          return;
+        }
 
         const modalReason = modalSubmit.fields.getTextInputValue(reasonId);
         const reason = modalReason.length < 1 ? commands.inactive.exec.kick.kickDefaultReason : modalReason;
-        const inactiveMember = inactiveMembersOnServer[currentPage - 1];
 
         await inactiveMember.send({
           embeds: [simpleEmbed(msgParams(commands.inactive.exec.kick.kickMessage, [reason]), "error")]
         });
         await inactiveMember.kick(msgParams(commands.inactive.exec.kick.kickReason, [member.user.username, reason]));
-
-        inactiveMembersOnServer.splice(currentPage - 1, 1);
-        newPageNum = currentPage;
+        inactiveMembersOnServer.splice(currentPage - 1, 1); // Remove kicked user from inactive members
+        inactiveMembersOnServer.splice(0, inactiveMembersOnServer.length);
 
         await modalSubmit.reply({
-          embeds: [simpleEmbed("C'est ban", "error")],
+          embeds: [simpleEmbed(msgParams(commands.inactive.exec.kick.memberKicked, [inactiveMember.id]))],
           ephemeral: true
         });
-      } catch (_) {
-        interaction.reply({
-          embeds: [simpleEmbed("erreur", "error")],
+
+        // No more inactive
+        if (inactiveMembersOnServer.length < 1) {
+          command.editReply({
+            embeds: [simpleEmbed(commands.inactive.exec.noInactiveMembers)],
+            components: []
+          });
+          return;
+        }
+
+        // Can't kick selected user
+      } catch (error) {
+        interaction.followUp({
+          embeds: [simpleEmbed(commands.inactive.exec.kick.errorWhileKicking, "error")],
           ephemeral: true
         });
         return;
@@ -103,34 +160,36 @@ export const execute: CommandExecute = async(command) => {
     }
 
     if (id === ids.inactiveFirst) newPageNum = 1;
-    if (id === ids.inactiveLast) newPageNum = inactiveMembersOnServer.length;
-    if (id === ids.inactiveNext) newPageNum = currentPage + 1;
     if (id === ids.inactivePrevious) newPageNum = currentPage - 1;
+    if (id === ids.inactiveNext) newPageNum = currentPage + 1;
+    if (id === ids.inactiveLast) newPageNum = inactiveMembersOnServer.length;
 
     const newPage = await getPage(inactiveMembersOnServer, newPageNum);
 
     if (!newPage) {
-    // Msg
+      command.editReply({
+        embeds: [simpleEmbed(commands.inactive.exec.memberQueryError, "error")],
+        components: []
+      });
       return;
     }
 
     pageNumberByMember.set(member.id, newPage.page);
 
-    const formattedPage = formatPage(newPage, member.permissions.has(PermissionFlagsBits.KickMembers));
+    const message = {
+      embeds: [getEmbed(newPage)],
+      components: [getActionRow(newPage, member.permissions.has(PermissionFlagsBits.KickMembers))]
+    };
 
-    if (!interaction.replied) {
-      interaction.update({
-        embeds: [formattedPage.embed],
-        components: [formattedPage.components]
-      });
+    // If interaction open kick reason modal
+    if (interaction.replied) {
+      command.editReply(message);
       return;
     }
 
-    command.editReply({
-      embeds: [formattedPage.embed],
-      components: [formattedPage.components]
-    });
+    interaction.update(message);
   });
+
 
   collector.on("end", () => {
     command.editReply({ components: [] });

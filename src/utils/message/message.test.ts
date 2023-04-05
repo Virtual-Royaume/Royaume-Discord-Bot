e3931f25564e99ed6a8d09611f96e6d25b9a93da
Bluzzi <camilledugas.pro@gmail.com>
2023-04-05 11:28:30 +0200

    message (util): improve tests

--- src/utils/message/message.test.ts
+++ src/utils/message/message.test.ts
@@ -1,40 +1,36 @@
 import { describe, it, expect } from "vitest";
-import { msgParams } from "./message";
+import { msgParams } from "./message.util";
 
-describe("msgParams", () => {
-  it("should replace the word", () => {
-    expect(msgParams("Hello {0}", ["World"])).toBe("Hello World");
+describe("msgParams function", () => {
+  it("should correctly replace placeholders with provided words", () => {
+    expect(msgParams("Hello {name}", ["Royaume"])).toBe("Hello Royaume");
   });
 
-  it("should replace the word with multiple params", () => {
-    expect(msgParams("Hello {0} {1}", ["World", "Vitest"])).toBe("Hello World Vitest");
+  it("should correctly replace multiple placeholders with provided words", () => {
+    expect(msgParams("Hello {name} and {otherName}", ["TS", "Vitest"])).toBe("Hello TS and Vitest");
   });
 
-  it("should replace the word with multiple params and multiple words", () => {
-    expect(msgParams("Hello {0} {1} {0}", ["World", "Vitest"])).not.toBe("Hello World Vitest World");
+  it("should correctly replace placeholders with provided words, ignoring duplicates", () => {
+    expect(msgParams("Hello {0} {1} {0}", ["World", "Vitest"])).toBe("Hello World Vitest {0}");
   });
 
-  it("should have more params than words", () => {
-    expect(msgParams("Hello {0}", ["World", "Vitest"])).toBe("Hello World");
+  it("should handle cases where there are more placeholders than words", () => {
+    expect(msgParams("Hello {name}", ["World", "Vitest"])).toBe("Hello World");
   });
 
-  it("should have less params than words", () => {
-    expect(msgParams("Hello {0} {1}", ["World"])).toBe("Hello World undefined");
+  it("should handle cases where there are fewer placeholders than words", () => {
+    expect(msgParams("Hello {0} {1}", ["World"])).toBe("Hello World {1}");
   });
 
-  it("should have no params", () => {
-    expect(msgParams("Hello {0}", [])).toBe("Hello undefined");
-  });
-
-  it("should not have any words", () => {
+  it("should handle cases where there are no placeholders", () => {
     expect(msgParams("Hello", ["World"])).toBe("Hello");
   });
 
-  it("should not have nothing", () => {
+  it("should handle cases where there is no text", () => {
     expect(msgParams("", [])).toBe("");
   });
 
-  it("should not have text", () => {
-    expect(msgParams("", ["Hello", "World"])).toBe("");
+  it("should ignore extra words when there are no placeholders", () => {
+    expect(msgParams("Hello", ["Hello", "World"])).toBe("Hello");
   });
 });

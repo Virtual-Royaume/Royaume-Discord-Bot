4e1a650d662de9c0248a7b64e9844b6d640b0221
Bluzzi <camilledugas.pro@gmail.com>
2023-04-06 00:38:22 +0200

    guild (util): create `getGuildMembers()` func

---
+++ src/utils/discord/guild.ts
@@ -0,0 +1,12 @@
+import { Collection, Guild, GuildMember } from "discord.js";
+
+export const getGuildMembers = async(guild: Guild): Promise<Collection<string, GuildMember> | null> => {
+  try {
+    const members = await guild.members.fetch({ time: 5000 });
+
+    if (members) return members;
+    return null;
+  } catch {
+    return null;
+  }
+};

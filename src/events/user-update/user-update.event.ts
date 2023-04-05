dbfe3057128911144bee811a8367295c45b3403c
Gaëtan H <gaetanhus@gmail.com>
2023-04-05 11:11:28 +0200

    user update (event): add info log

--- src/events/user-update/user-update.event.ts
+++ src/events/user-update/user-update.event.ts
@@ -1,6 +1,8 @@
 import { EventExecute, EventName } from "$core/utils/handler/event";
 import { gqlRequest } from "$core/utils/request";
 import { setUsernameAndProfilePicture } from "$core/api/requests/member";
+import { logger } from "$core/utils/logger";
+import { userWithId } from "$core/utils/user";
 
 export const event: EventName = "userUpdate";
 
@@ -10,4 +12,6 @@ export const execute: EventExecute<"userUpdate"> = async(_, newUser) => {
     username: newUser.username,
     profilePicture: newUser.displayAvatarURL()
   });
+
+  logger.info(`User ${userWithId(newUser)} updated`);
 };

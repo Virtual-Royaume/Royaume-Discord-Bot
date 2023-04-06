9bdd53dba856c6b4c48dd19e4a37f22e737253a7
Bluzzi <camilledugas.pro@gmail.com>
2023-04-06 20:47:11 +0200

    request (util): fix `gqlRequest()` error

--- src/utils/request/request.ts
+++ src/utils/request/request.ts
@@ -73,7 +73,8 @@ export const gqlRequest = async<D, V>(document: TypedDocumentNode<D, V>, variabl
 
   if (!response.ok) {
     logger.error("GraphQL request failed :");
-    console.log(await response.json());
+    console.log(JSON.stringify(document, null, 2));
+    console.log(await response.text());
 
     return {
       success: false

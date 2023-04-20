8cece94e0d884ca028a2109c7d8c722e70bc6b11
Bluzzi <camilledugas.pro@gmail.com>
2023-04-20 14:48:29 +0200

    refactor(event.type.ts): add MaybePromise type to EventExecute function to allow async event handlers

--- src/utils/handler/event/event.type.ts
+++ src/utils/handler/event/event.type.ts
@@ -1,7 +1,8 @@
+import type { MaybePromise } from "$core/utils/typing/promise";
 import type { ClientEvents } from "discord.js";
 
 export type EventName = keyof ClientEvents;
 
 export type EventOnce = boolean;
 
-export type EventExecute<K extends EventName> = (...args: ClientEvents[K]) => void;
+export type EventExecute<K extends EventName> = (...args: ClientEvents[K]) => MaybePromise<void>;

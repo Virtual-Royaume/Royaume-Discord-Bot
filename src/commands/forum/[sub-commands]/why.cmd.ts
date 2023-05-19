f1cfe9a19d14fd73604843a12ab49525fcab6622
Gaëtan H <steellgold0@gmail.com>
2023-05-19 10:45:19 +0200

    refactor(why.cmd.ts): remove unnecessary null check in execute function

--- src/commands/forum/[sub-commands]/why.cmd.ts
+++ src/commands/forum/[sub-commands]/why.cmd.ts
@@ -7,7 +7,7 @@ export const execute: CommandExecute = (command) => {
   const user = command.options.getUser(commands.forum.subcmds.why.options.user.name, false);
 
   void command.reply({
-    content: user !== null ? msgParams(commands.forum.exec.why.user, [user.id]) : undefined,
+    content: user ? msgParams(commands.forum.exec.why.user, [user.id]) : undefined,
     embeds: [simpleEmbed(msgParams(commands.forum.exec.why.message, [command.channelId]))]
   });
 };

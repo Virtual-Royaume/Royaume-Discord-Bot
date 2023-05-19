193e5ebdd0ca7ffd9a80d36dd369ed06795f4184
Gaëtan H <steellgold0@gmail.com>
2023-05-19 10:38:28 +0200

    refactor(why.cmd.ts): simplify user variable assignment and remove unnecessary ternary operator

--- src/commands/forum/[sub-commands]/why.cmd.ts
+++ src/commands/forum/[sub-commands]/why.cmd.ts
@@ -4,11 +4,10 @@ import { commands } from "$core/configs/message/command";
 import { msgParams } from "$core/utils/message";
 
 export const execute: CommandExecute = (command) => {
-  let user = command.options.getUser(commands.forum.subcmds.why.options.user.name, false);
-  if (!user) user = command.user;
+  const user = command.options.getUser(commands.forum.subcmds.why.options.user.name, false);
 
   void command.reply({
-    content: user !== command.user ? msgParams(commands.forum.exec.why.user, [user.id]) : undefined,
+    content: user !== null ? msgParams(commands.forum.exec.why.user, [user.id]) : undefined,
     embeds: [simpleEmbed(msgParams(commands.forum.exec.why.message, [command.channelId]))]
   });
 };

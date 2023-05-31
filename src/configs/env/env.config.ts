60caeff0b21efa88d7586f5bbccfeefd2ef82b8f
Bluzzi <camilledugas.pro@gmail.com>
2023-05-31 07:55:34 +0200

    fix(env.config.ts): improve error message when environment variables are missing by including the cause of the error

--- src/configs/env/env.config.ts
+++ src/configs/env/env.config.ts
@@ -4,7 +4,7 @@ import { envDTO } from "./env.dto";
 // Variables:
 const parser = envDTO.safeParse(process.env);
 
-if (!parser.success) throw Error("tgm");
+if (!parser.success) throw Error("Missing environment variable", { cause: parser.error.message });
 
 export const env = parser.data;
 

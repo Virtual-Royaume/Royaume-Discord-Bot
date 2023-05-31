28a0d5143e16f8be1a76f9d08afddb4ddc62e4c5
Bluzzi <camilledugas.pro@gmail.com>
2023-05-31 10:27:07 +0200

    fix(graphql.ts): change 'value' to 'data' in gqlRequest response to match GraphQL spec and improve semantics

--- src/utils/request/graphql/graphql.ts
+++ src/utils/request/graphql/graphql.ts
@@ -24,5 +24,5 @@ export const gqlRequest = async<D, V>(document: TypedDocumentNode<D, V>, variabl
     return error(Error("GraphQL request failed"));
   }
 
-  return ok((await response.json()).value)
+  return ok((await response.json()).data)
 };

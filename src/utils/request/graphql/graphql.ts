57715bc1652c86de2c4486fe814d57e06faf8a91
Bluzzi <camilledugas.pro@gmail.com>
2023-05-31 08:07:12 +0200

    refactor(graphql.ts): remove unnecessary console.log statement from gqlRequest function to improve code readability

--- src/utils/request/graphql/graphql.ts
+++ src/utils/request/graphql/graphql.ts
@@ -20,7 +20,6 @@ export const gqlRequest = async<D, V>(document: TypedDocumentNode<D, V>, variabl
   if (!response.ok) {
     logger.error("GraphQL request failed :");
     console.log(JSON.stringify(document, null, 2));
-    console.log(await response.text());
 
     return error(Error("GraphQL request failed"));
   }

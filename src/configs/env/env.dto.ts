e37eadb45874e30ce70f23a3694f7faafd5b3f31
Bluzzi <camilledugas.pro@gmail.com>
2023-06-14 01:39:23 +0200

    refactor(env): rename GITHUB_TOKEN to GH_TOKEN to improve semantics and consistency with other variables

--- src/configs/env/env.dto.ts
+++ src/configs/env/env.dto.ts
@@ -4,5 +4,5 @@ export const envDTO = z.object({
   API_LINK: z.string().url(),
   API_TOKEN: z.string().uuid(),
   BOT_TOKEN: z.string().nonempty(),
-  GITHUB_TOKEN: z.string().nonempty()
+  GH_TOKEN: z.string().nonempty()
 });

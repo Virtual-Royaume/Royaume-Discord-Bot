f8739da0692dafa692810ed3b613e9a527b71317
Bluzzi <camilledugas.pro@gmail.com>
2023-06-14 01:11:17 +0200

    refactor(message.util.ts): replace double quotes with single quotes in message parameter of msgParams function to improve consistency with codebase

--- src/utils/message/message.util.ts
+++ src/utils/message/message.util.ts
@@ -1,5 +1,5 @@
 export const msgParams = (message: string, params: (string | number)[]): string => {
-  const words = message.match(/\{[^}]+\}/g);
+  const words = message.match(/{[^}]+}/g);
 
   if (words) for (let i = 0; i < params.length; i++) {
     message = message.replace(words[i], String(params[i]));

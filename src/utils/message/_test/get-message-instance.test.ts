19721b461728a8a5b7f9d71b05c29505f4e61d6d
Gaëtan H <steellgold0@gmail.com>
2023-06-20 16:40:34 +0200

    test(get-message-instance.test.ts): add tests for PTB and Canary links and non-discord links

--- src/utils/message/_test/get-message-instance.test.ts
+++ src/utils/message/_test/get-message-instance.test.ts
@@ -7,4 +7,19 @@ describe("isDiscordLink function", () => {
     const message = await getMessageFromLink("https://discord.com/channels/732251741999071303/786216771723198514/803532192793493544");
     expect(message).toBeInstanceOf(Message);
   });
+
+  it("should return instance of Message with PTB link", async() => {
+    const message = await getMessageFromLink("https://ptb.discord.com/channels/732251741999071303/786216771723198514/803532192793493544");
+    expect(message).toBeInstanceOf(Message);
+  });
+
+  it("should return instance of Message with Canary link", async() => {
+    const message = await getMessageFromLink("https://canary.discord.com/channels/732251741999071303/786216771723198514/803532192793493544");
+    expect(message).toBeInstanceOf(Message);
+  });
+
+  it("should return null when the link is not a discord link", async() => {
+    const message = await getMessageFromLink("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    expect(message).toBe(null);
+  });
 });

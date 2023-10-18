297a8ff944de9179ab5f8e842f3290c52f6ccf40
Gaëtan H <steellgold0@gmail.com>
2023-10-18 18:19:33 +0000

    twitter preview

---
+++ src/events/twitter-link-reaction/twitter-link-reaction.event.ts
@@ -0,0 +1,18 @@
+import type { EventExecute, EventName } from "#/utils/handler/event";
+import { twitterLinkRegex, extractTwitterLink } from "./twitter-link-reaction.const";
+
+export const enableInDev = true;
+
+export const event: EventName = "messageCreate";
+
+export const execute: EventExecute<"messageCreate"> = async(message) => {
+  if (message.author.bot) return;
+  if (!twitterLinkRegex.test(message.content)) return;
+
+  const twitterLink = extractTwitterLink(message.content);
+  if (!twitterLink) return;
+
+  const links = Array.isArray(twitterLink) ? twitterLink : [twitterLink];
+  const replacedLinks = links.map(link => link.replace(/x\.com/g, "vxtwitter.com").replace(/twitter\.com/g, "vxtwitter.com"));
+  await message.reply(replacedLinks.join("\n"));
+};

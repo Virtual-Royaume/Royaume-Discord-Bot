640c5b6496fab2ee2e8ebf279dc4d4ac726b28a4
Gaëtan H <steellgold0@gmail.com>
2023-10-18 18:25:04 +0000

    esfix

--- src/events/twitter-link-reaction/twitter-link-reaction.const.ts
+++ src/events/twitter-link-reaction/twitter-link-reaction.const.ts
@@ -1,6 +1,6 @@
-export const twitterLinkRegex = /https?:\/\/(?:twitter\.com|x\.com)\/(?:#!\/)?(\w+)\/status?\/(\d+)/g
+export const twitterLinkRegex = /https?:\/\/(?:twitter\.com|x\.com)\/(?:#!\/)?(\w+)\/status?\/(\d+)/g;
 
 export const extractTwitterLink = (text: string): string | string[] | null => {
   if (!twitterLinkRegex.test(text)) return null;
   return text.match(twitterLinkRegex);
-}
+};
